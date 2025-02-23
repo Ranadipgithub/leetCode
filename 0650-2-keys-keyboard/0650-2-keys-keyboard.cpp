0d85e97c510ac8733603c63c5a8e18af5749b566
Rana Dutta <duttarana189@gmail.com>
2025-02-24 01:24:34 +0530

    Time: 4 ms (50.53%), Space: 9.5 MB (37.52%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,29 +1,50 @@
 class Solution {
 private:
-    vector<vector<int>> dp; 
+    // // vector<vector<int>> dp; 
 
-    int solve(int n, int currA, int clip, int count) {
-        if (currA > n) return INT_MAX;
-        if (currA == n) return count;  
+    // int solve(int n, int currA, int clip, int count) {
+    //     if (currA > n) return INT_MAX;
+    //     if (currA == n) return count;  
         
-        if (dp[currA][clip] != -1) return dp[currA][clip];
+    //     if (dp[currA][clip] != -1) return dp[currA][clip];
 
-        int copyAndPaste = INT_MAX, paste = INT_MAX;
+    //     int copyAndPaste = INT_MAX, paste = INT_MAX;
         
-        if (clip != currA) {
-            copyAndPaste = solve(n, currA * 2, currA, count + 2);
-        }
+    //     if (clip != currA) {
+    //         copyAndPaste = solve(n, currA * 2, currA, count + 2);
+    //     }
 
-        if (clip > 0) {
-            paste = solve(n, currA + clip, clip, count + 1);
-        }
+    //     if (clip > 0) {
+    //         paste = solve(n, currA + clip, clip, count + 1);
+    //     }
 
-        return dp[currA][clip] = min(copyAndPaste, paste);
-    }
+    //     return dp[currA][clip] = min(copyAndPaste, paste);
+    // }
 
 public:
     int minSteps(int n) {
-        dp.assign(n + 1, vector<int>(n + 1, -1)); 
-        return solve(n, 1, 0, 0); 
+        // dp.assign(n + 1, vector<int>(n + 1, -1)); 
+        // return solve(n, 1, 0, 0); 
+        if(n == 0) return 0;
+        if(n == 1) return 0;
+        vector<int> dp(n+1, 0);
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 2;
+        for(int i = 3;i<=n;i++){
+            int factor = i/2;
+            while(factor > 0){
+                if(i % factor == 0){
+                int steps = dp[factor];
+                int copy = 1;
+                int paste = (i / factor) - 1;
+                dp[i] = steps + copy + paste;
+                break;
+                }
+                else factor--;
+            }
+            
+        }
+        return dp[n];
     }
 };

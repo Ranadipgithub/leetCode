d909605fa1cfc2bb87e3c28205a79ef4367f4a94
Rana Dutta <duttarana189@gmail.com>
2025-05-08 14:30:16 +0530

    Time: 0 ms (100%), Space: 14.7 MB (64.19%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -4,31 +4,61 @@ public:
         int n = nums1.size();
         int m = nums2.size();
 
-        map<int, int> mpp;
-        vector<vector<int>> result;
+        vector<vector<int>> res;
 
-        for(int i = 0;i<n;i++){
-            int id = nums1[i][0];
-            int val = nums1[i][1];
+        int i = 0, j = 0;
 
-            mpp[id] += val;
+        while(i<n && j<m){
+            if(nums1[i][0] < nums2[j][0]){
+                res.push_back(nums1[i]);
+                i++;
+            } else if(nums1[i][0] > nums2[j][0]){
+                res.push_back(nums2[j]);
+                j++;
+            } else{
+                res.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++;
+            }
         }
 
-        for(int i = 0;i<m;i++){
-            int id = nums2[i][0];
-            int val = nums2[i][1];
-
-            mpp[id] += val;
+        while(i < n){
+            res.push_back(nums1[i]);
+            i++;
         }
 
-        for(auto &it: mpp){
-            int key = it.first;
-            int val = it.second;
-
-            result.push_back({key, val});
+        while(j < m){
+            res.push_back(nums2[j]);
+            j++;
         }
 
-        // sort(begin(result), end(result));
-        return result;
+        return res;
+
+        // map<int, int> mpp;
+        // vector<vector<int>> result;
+
+        // for(int i = 0;i<n;i++){
+        //     int id = nums1[i][0];
+        //     int val = nums1[i][1];
+
+        //     mpp[id] += val;
+        // }
+
+        // for(int i = 0;i<m;i++){
+        //     int id = nums2[i][0];
+        //     int val = nums2[i][1];
+
+        //     mpp[id] += val;
+        // }
+
+        // for(auto &it: mpp){
+        //     int key = it.first;
+        //     int val = it.second;
+
+        //     result.push_back({key, val});
+        // }
+
+        // // sort(begin(result), end(result));
+        // return result;
     }
 };

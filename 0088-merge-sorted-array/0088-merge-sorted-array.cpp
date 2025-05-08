816ade12d36f77e9b3a851883f162bf22b0259ea
Rana Dutta <duttarana189@gmail.com>
2025-05-08 14:42:14 +0530

    Time: 0 ms (100%), Space: 12.2 MB (95.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,29 +2,40 @@ class Solution {
 private:
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0, j = 0;
-        vector<int> res;
-        while(i<m && j < n){
-            if(nums1[i] <= nums2[j]){
-                res.push_back(nums1[i]);
-                i++;
+        int i = m-1, j = n-1, k = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
             } else{
-                res.push_back(nums2[j]);
-                j++;
-            } 
+                nums1[k--] = nums2[j--];
+            }
         }
-        while(i < m){
-            res.push_back(nums1[i]);
-            i++;
+        while(j>=0){
+            nums1[k--] = nums2[j--];
         }
-        while(j < n){
-            res.push_back(nums2[j]);
-            j++;
-        }
+        // int i = 0, j = 0;
+        // vector<int> res;
+        // while(i<m && j < n){
+        //     if(nums1[i] <= nums2[j]){
+        //         res.push_back(nums1[i]);
+        //         i++;
+        //     } else{
+        //         res.push_back(nums2[j]);
+        //         j++;
+        //     } 
+        // }
+        // while(i < m){
+        //     res.push_back(nums1[i]);
+        //     i++;
+        // }
+        // while(j < n){
+        //     res.push_back(nums2[j]);
+        //     j++;
+        // }
 
-        for(int i = 0;i<m+n;i++){
-            nums1[i] = res[i];
-        }
+        // for(int i = 0;i<m+n;i++){
+        //     nums1[i] = res[i];
+        // }
         // int left = m-1;
         // int right = 0;
         // while(left >= 0 && right < n){

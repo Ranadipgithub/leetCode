301a8f58b6583314a1c8ad59df0c49a20b7d420c
Rana Dutta <duttarana189@gmail.com>
2025-05-10 23:35:41 +0530

    Time: 8 ms (6.22%), Space: 42.3 MB (9.05%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,32 +2,45 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> res(n, 0);
-        int zeroCount = 0;
-        int product = 1;
-
-        for (int num : nums) {
-            if (num == 0) {
-                zeroCount++;
-            } else {
-                product *= num;
-            }
+        vector<int>pre(n,1);
+        for(int i = 1;i<n;i++){
+            pre[i] = pre[i-1]*nums[i-1];
         }
-
-        if (zeroCount > 1) {
-            return res;
+        vector<int>suff(n+1, 1);
+        for(int i = n-2;i>=0;i--){
+            suff[i] = suff[i+1]*nums[i+1];
         }
-
-        for (int i = 0; i < n; ++i) {
-            if (zeroCount == 1) {
-                if (nums[i] == 0) {
-                    res[i] = product;
-                } 
-            } else {
-                res[i] = product / nums[i];
-            }
+        vector<int>res(n,0);
+        for(int i = 0;i<n;i++){
+            res[i] = pre[i]*suff[i];
         }
-
         return res;
+        // vector<int> res(n, 0);
+        // int zeroCount = 0;
+        // int product = 1;
+
+        // for (int num : nums) {
+        //     if (num == 0) {
+        //         zeroCount++;
+        //     } else {
+        //         product *= num;
+        //     }
+        // }
+
+        // if (zeroCount > 1) {
+        //     return res;
+        // }
+
+        // for (int i = 0; i < n; ++i) {
+        //     if (zeroCount == 1) {
+        //         if (nums[i] == 0) {
+        //             res[i] = product;
+        //         } 
+        //     } else {
+        //         res[i] = product / nums[i];
+        //     }
+        // }
+
+        // return res;
     }
 };

76e0750890af8891ed11f7878b78d2329eba7763
Rana Dutta <duttarana189@gmail.com>
2025-05-15 12:33:05 +0530

    Time: 0 ms (100%), Space: 22 MB (47.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -18,16 +18,25 @@ public:
         res.push_back(root->val);
         inOrder(root->right, res);
     }
+    TreeNode *prev = nullptr;
+    bool inorder(TreeNode *root){
+        if(!root) return true;
+        if(!inorder(root->left)) return false;
+        if(prev && root->val <= prev->val) return false;
+        prev = root;
+        return inorder(root->right);
+    }
     bool isValidBST(TreeNode* root) {
-        vector<int> res;
-        inOrder(root, res);
-        bool fg = true;
-        for(int i = 1;i<res.size();i++){
-            if(res[i-1]>=res[i]){
-                fg = false;
-                break;
-            }
-        }
-        return fg;
+        return inorder(root);
+        // vector<int> res;
+        // inOrder(root, res);
+        // bool fg = true;
+        // for(int i = 1;i<res.size();i++){
+        //     if(res[i-1]>=res[i]){
+        //         fg = false;
+        //         break;
+        //     }
+        // }
+        // return fg;
     }
 };

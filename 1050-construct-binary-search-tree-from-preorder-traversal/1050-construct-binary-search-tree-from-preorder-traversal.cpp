d1e12eea8312d86d6be7467c32fdb6233cf6b75c
Rana Dutta <duttarana189@gmail.com>
2025-05-15 14:19:46 +0530

    Time: 0 ms (100%), Space: 16.4 MB (69.23%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,25 +11,36 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& preorder, int start, int end) {
-        if (start > end) return nullptr;
+    // TreeNode* solve(vector<int>& preorder, int start, int end) {
+    //     if (start > end) return nullptr;
 
-        TreeNode* root = new TreeNode(preorder[start]);
+    //     TreeNode* root = new TreeNode(preorder[start]);
 
-        int rightSubtreeStart = end + 1;
-        for (int i = start + 1; i <= end; i++) {
-            if (preorder[i] > preorder[start]) {
-                rightSubtreeStart = i;
-                break;
-            }
+    //     int rightSubtreeStart = end + 1;
+    //     for (int i = start + 1; i <= end; i++) {
+    //         if (preorder[i] > preorder[start]) {
+    //             rightSubtreeStart = i;
+    //             break;
+    //         }
+    //     }
+
+    //     root->left = solve(preorder, start + 1, rightSubtreeStart - 1);
+    //     root->right = solve(preorder, rightSubtreeStart, end);
+    //     return root;
+    // }
+    TreeNode *solve(vector<int>&preorder, int lower, int upper, int &index){
+        if(index == preorder.size() || preorder[index] < lower || preorder[index] > upper){
+            return nullptr;
         }
-
-        root->left = solve(preorder, start + 1, rightSubtreeStart - 1);
-        root->right = solve(preorder, rightSubtreeStart, end);
+        TreeNode *root = new TreeNode(preorder[index++]);
+        root->left = solve(preorder, lower, root->val, index);
+        root->right = solve(preorder, root->val, upper, index);
         return root;
     }
 
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        return solve(preorder, 0, preorder.size() - 1);
+        // return solve(preorder, 0, preorder.size() - 1);
+        int idx = 0;
+        return solve(preorder, INT_MIN, INT_MAX, idx);
     }
 };

4955cc4be294855e499748020166428cc622f1ad
Rana Dutta <duttarana189@gmail.com>
2025-05-16 15:01:15 +0530

    Time: 4 ms (60.3%), Space: 102 MB (65.92%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -20,14 +20,33 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         int peri = 0;
+        // for(int i = 0;i<n;i++){
+        //     for(int j = 0;j<m;j++){
+        //         if(grid[i][j] == 1){
+        //             dfs(grid, i, j, peri);
+        //             return peri;
+        //         }
+        //     }
+        // }
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(grid[i][j] == 1){
-                    dfs(grid, i, j, peri);
-                    return peri;
-                }
+                if(grid[i][j] == 0) continue;
+                if(i - 1 < 0 || grid[i - 1][j] == 0)
+                peri++;
+
+            // Check down
+            if(i + 1 >= n || grid[i + 1][j] == 0)
+                peri++;
+
+            // Check left
+            if(j - 1 < 0 || grid[i][j - 1] == 0)
+                peri++;
+
+            // Check right
+            if(j + 1 >= m || grid[i][j + 1] == 0)
+                peri++;
             }
         }
-        return -1;
+        return peri;
     }
 };

2dafbe69fdc8d0d4b37d0d3ea28aa3bf4dcfe0c4
Rana Dutta <duttarana189@gmail.com>
2025-05-18 11:07:11 +0530

    Time: 0 ms (100%), Space: 17.7 MB (14.29%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -13,12 +13,31 @@ public:
         }
         return true;
     }
+    bool bfs(vector<vector<int>>& graph, int node, vector<int>&color, int currColor){
+        queue<int> q;
+        q.push(node);
+        color[node] = currColor;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int &v: graph[u]){
+                if(color[v] == color[u]){
+                    return false;
+                }
+                if(color[v] == -1){
+                    color[v] = 1-color[u];
+                    q.push(v);
+                }
+            }
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> color(n,-1);
         for(int i = 0;i<n;i++){
             if(color[i] == -1){
-                if(!dfs(graph, i, color, 1)){
+                if(!bfs(graph, i, color, 1)){
                     return false;
                 }
             }

1a3d7142a9825896795146a1a6b13158893b2636
Rana Dutta <duttarana189@gmail.com>
2025-05-23 16:48:05 +0530

    Time: 173 ms (9.82%), Space: 86.7 MB (10.14%) - LeetHub

--- 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.cpp
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.cpp
@@ -2,26 +2,47 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         int n = arr.size();
-        map<int, int> freq;
-        for (int num : arr) {
-            freq[num]++;
+        map<int, int> mpp;
+        for(int i = 0;i<n;i++){
+            mpp[arr[i]]++;
         }
-
-        vector<pair<int, int>> freqVec(freq.begin(), freq.end());
-
-        sort(freqVec.begin(), freqVec.end(), [](pair<int, int> a, pair<int, int> b) {
-            return a.second > b.second;  
-        });
-
+        priority_queue<pair<int, int>> pq;
+        for(auto it: mpp){
+            pq.push({it.second, it.first});
+        }
         int cnt = 0;
-        int removed = 0;
-        for (auto& p : freqVec) {
-            removed += p.second;
+        int freq = 0;
+        while(!pq.empty()){
+            int el = pq.top().second;
+            freq += pq.top().first;
+            pq.pop();
             cnt++;
-            if((n - removed) <= n/2) break;
-            
+            if((n - freq) <= n/2){
+                return cnt;
+            }
         }
-        return cnt;
+        return -1;
+        // int n = arr.size();
+        // map<int, int> freq;
+        // for (int num : arr) {
+        //     freq[num]++;
+        // }
+
+        // vector<pair<int, int>> freqVec(freq.begin(), freq.end());
+
+        // sort(freqVec.begin(), freqVec.end(), [](pair<int, int> a, pair<int, int> b) {
+        //     return a.second > b.second;  
+        // });
+
+        // int cnt = 0;
+        // int removed = 0;
+        // for (auto& p : freqVec) {
+        //     removed += p.second;
+        //     cnt++;
+        //     if((n - removed) <= n/2) break;
+            
+        // }
+        // return cnt;
         
         // set<int> st;
         // for(int i = 0;i<n;i++){

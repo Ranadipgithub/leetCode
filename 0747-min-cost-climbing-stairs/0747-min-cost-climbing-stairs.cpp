f77912e80ab961d96910e1d0c88ff528c6cfabd7
Rana Dutta <duttarana189@gmail.com>
2025-05-30 13:46:47 +0530

    Time: 0 ms (100%), Space: 17.3 MB (96.99%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -10,7 +10,12 @@ public:
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        memset(t, -1, sizeof(t));
-        return min(solve(0, cost), solve(1, cost));
+        // memset(t, -1, sizeof(t));
+        // return min(solve(0, cost), solve(1, cost));
+        if(n == 2) return min(cost[0], cost[1]);
+        for(int i = 2;i<n;i++){
+            cost [i] = cost[i] +  min(cost[i-1], cost[i-2]);
+        }
+        return min(cost[n-1], cost[n-2]);
     }
 };

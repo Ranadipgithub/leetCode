369ee073795660a49a4c8dd601b8527c50aacc92
Rana Dutta <duttarana189@gmail.com>
2025-05-31 09:05:05 +0530

    Time: 228 ms (50.59%), Space: 156.9 MB (49.41%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, bool isEven, int idx){
+    vector<vector<long long>>dp;
+    long long solve(vector<int>&nums, bool isEven, long long idx){
         if(idx >= nums.size()) return 0;
-        int take = solve(nums, !isEven, idx+1) + (isEven? nums[idx]: -nums[idx]);
-        
-        int skip = solve(nums, isEven, idx+1);
-        return max(take, skip);
+        if(dp[idx][isEven] != -1) return dp[idx][isEven];
+        long long take = solve(nums, !isEven, idx+1) + (isEven? nums[idx]: -nums[idx]);
+        long long skip = solve(nums, isEven, idx+1);
+        return dp[idx][isEven] = max(take, skip);
     }
     long long maxAlternatingSum(vector<int>& nums) {
-        int n = nums.size();
-        return solve(nums, true, 0);
+        long long n = nums.size();
+        // dp = vector<vector<long long>>(n, vector<long long>(2, -1));
+        // return solve(nums, true, 0);
+        vector<vector<long long>>t(n+1, vector<long long>(2, 0));
+        for(int i = 1;i<n+1;i++){
+            t[i][0] = max(t[i-1][1] - nums[i-1], t[i-1][0]);
+            t[i][1] = max(t[i-1][0] + nums[i-1], t[i-1][1]);
+        }
+        return max(t[n][0], t[n][1]);
     }
 };

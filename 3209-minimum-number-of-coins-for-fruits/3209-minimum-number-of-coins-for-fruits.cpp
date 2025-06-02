d97da65bacd21a53e0956515f8a3d8689d66cde0
Rana Dutta <duttarana189@gmail.com>
2025-06-02 15:31:51 +0530

    Time: 55 ms (34.45%), Space: 94.3 MB (24.08%) - LeetHub

---
+++ 3209-minimum-number-of-coins-for-fruits/3209-minimum-number-of-coins-for-fruits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int solve(const vector<int>& prices, int i, int lastIdx) {
+        int n = prices.size();
+        if (i > n) return 0;
+
+        if (dp[i][lastIdx] != -1) 
+            return dp[i][lastIdx];
+
+        if (i > lastIdx) {
+            int nextLast = min(n, 2 * i);
+            return dp[i][lastIdx] = prices[i - 1] + solve(prices, i + 1, nextLast);
+        }
+        int takeFree = solve(prices, i + 1, lastIdx);
+        int extended = min(n, max(lastIdx, 2 * i));
+        int buyAnyway = prices[i - 1] + solve(prices, i + 1, extended);
+        return dp[i][lastIdx] = min(takeFree, buyAnyway);
+    }
+
+    int minimumCoins(vector<int>& prices) {
+        int n = prices.size();
+        dp.assign(n + 1, vector<int>(n + 1, -1));
+        return solve(prices, 1, 0);
+    }
+};

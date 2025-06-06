701a39da1f166d939e7f151548d69a90bbaaddb0
Rana Dutta <duttarana189@gmail.com>
2025-06-06 14:29:50 +0530

    Time: 116 ms (40.57%), Space: 38.7 MB (35.21%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -18,7 +18,19 @@ public:
     }
 
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp)); 
-        return solve(nums, 0, -1);
+        // memset(dp, -1, sizeof(dp)); 
+        // return solve(nums, 0, -1);
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int maxi = 1;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<i;j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                    maxi = max(maxi, dp[i]);
+                }
+            }
+        }
+        return maxi;
     }
 };

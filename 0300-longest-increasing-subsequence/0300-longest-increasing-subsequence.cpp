bc0bb6fb84c1a1ba001a3dcc01bbbe502cb737ce
Rana Dutta <duttarana189@gmail.com>
2025-06-06 14:50:59 +0530

    Time: 49 ms (72.03%), Space: 38.3 MB (35.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -21,16 +21,28 @@ public:
         // memset(dp, -1, sizeof(dp)); 
         // return solve(nums, 0, -1);
         int n = nums.size();
-        vector<int> dp(n, 1);
-        int maxi = 1;
+        // vector<int> dp(n, 1);
+        // int maxi = 1;
+        // for(int i = 0;i<n;i++){
+        //     for(int j = 0;j<i;j++){
+        //         if(nums[i] > nums[j]){
+        //             dp[i] = max(dp[i], 1 + dp[j]);
+        //             maxi = max(maxi, dp[i]);
+        //         }
+        //     }
+        // }
+        // return maxi;
+
+        // using patience sorting
+        vector<int> sorted;
         for(int i = 0;i<n;i++){
-            for(int j = 0;j<i;j++){
-                if(nums[i] > nums[j]){
-                    dp[i] = max(dp[i], 1 + dp[j]);
-                    maxi = max(maxi, dp[i]);
-                }
+            auto it = lower_bound(begin(sorted), end(sorted), nums[i]); // just greater than or equal to
+            if(it == end(sorted)){
+                sorted.push_back(nums[i]);
+            } else{
+                *it = nums[i];
             }
         }
-        return maxi;
+        return sorted.size();
     }
 };

61c1c8957e54af465a32f70f21f47583dfdf3947
Rana Dutta <duttarana189@gmail.com>
2025-06-08 07:34:41 +0530

    Time: 0 ms (100%), Space: 14 MB (69.54%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    vector<int> lexicalOrder(int n) {
-        vector<string> v;
-        for (int i = 1; i <= n; i++) {
-            v.push_back(to_string(i));
+    void solve(int curr, int n, vector<int>&res){
+        if(curr > n) return;
+        res.push_back(curr);
+        for(int append = 0;append<=9;append++){
+            int temp = (curr*10)+append;
+            if(temp > n) return;
+            solve(temp, n, res);
         }
-        sort(v.begin(), v.end());
-
+    }
+    vector<int> lexicalOrder(int n) {
         vector<int> res;
-        for (int i = 0; i < n; i++) { 
-            res.push_back(stoi(v[i]));
+        for(int i = 1;i<=9;i++){
+            solve(i, n, res);
         }
         return res;
+        // vector<string> v;
+        // for (int i = 1; i <= n; i++) {
+        //     v.push_back(to_string(i));
+        // }
+        // sort(v.begin(), v.end());
+
+        // vector<int> res;
+        // for (int i = 0; i < n; i++) { 
+        //     res.push_back(stoi(v[i]));
+        // }
+        // return res;
     }
 };

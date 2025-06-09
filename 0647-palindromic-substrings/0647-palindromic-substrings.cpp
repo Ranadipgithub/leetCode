b471199dce7358b9e75cddf13ba2809373dbb029
Rana Dutta <duttarana189@gmail.com>
2025-06-09 08:32:22 +0530

    Time: 27 ms (32.09%), Space: 11.1 MB (52.84%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -10,12 +10,29 @@ public:
         }
         return true;
     }
+   
     int countSubstrings(string s) {
         int n = s.size();
         int cnt = 0;
-        for(int i = 0;i<n;i++){
-            for(int j = i;j<n;j++){
-                if(checkPalindrome(s, i, j)) cnt++;
+        // for(int i = 0;i<n;i++){
+        //     for(int j = i;j<n;j++){
+        //         if(checkPalindrome(s, i, j)) cnt++;
+        //     }
+        // }
+
+        vector<vector<bool>>t(n, vector<bool>(n, 0));
+        for(int l = 1;l<=n;l++){
+            for(int i = 0; i+l-1 < n;i++){
+                int j = i+l-1;
+                if(i == j){
+                    t[i][j] = true;
+                } else if(i+1 == j){
+                    t[i][j] = (s[i] == s[j]);
+                } else{
+                    t[i][j] = s[i] == s[j] && t[i+1][j-1];
+                }
+
+                if(t[i][j]) cnt++;
             }
         }
         return cnt;

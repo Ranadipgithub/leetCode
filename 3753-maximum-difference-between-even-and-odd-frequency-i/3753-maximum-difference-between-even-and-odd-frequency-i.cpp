bb2c4b06b914886845ec369978011bc712487d23
Rana Dutta <duttarana189@gmail.com>
2025-06-10 08:05:40 +0530

    Time: 0 ms (100%), Space: 10 MB (16.7%) - LeetHub

---
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        int n = s.size();
+        unordered_map<char, int> fq;
+        for(int i = 0;i<n;i++){
+            fq[s[i]]++;
+        }
+        int maxiOdd = 0, maxiEven = 0, miniEven = INT_MAX, res = INT_MIN;
+        for(auto it: fq){
+            if(it.second%2 == 1) maxiOdd = max(maxiOdd, it.second);
+            else{
+                maxiEven = max(maxiEven, it.second);
+                miniEven = min(miniEven, it.second);
+            } 
+            
+        }
+        return max(maxiOdd-miniEven, maxiOdd-maxiEven);
+    }
+};

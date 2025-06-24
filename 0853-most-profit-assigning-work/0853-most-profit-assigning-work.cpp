9da76a6064524e3c2520018cf71b3ed7679aefd4
Rana Dutta <duttarana189@gmail.com>
2025-06-24 20:56:24 +0530

    Time: 20 ms (35.1%), Space: 41.7 MB (60.95%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -4,16 +4,36 @@ public:
         int n = worker.size();
         int m = difficulty.size();
 
+        priority_queue<pair<int, int>>pq;
+        for(int i = 0;i<m;i++){
+            pq.push({profit[i], difficulty[i]});
+        }
+
         int total = 0;
+        sort(worker.begin(), worker.end(), greater<int>());
         for(int i = 0;i<n;i++){
-            int currProfit = 0;
-            for(int j = 0;j<m;j++){
-                if(difficulty[j] <= worker[i]){
-                    currProfit = max(currProfit, profit[j]);
-                }
+            while(!pq.empty() && pq.top().second > worker[i]) pq.pop();
+            if(pq.top().second <= worker[i]){
+                total += pq.top().first;
             }
-            total += currProfit;
+            // if(pq.top().second <= worker[i]){
+            //     total += pq.top().first;
+            // } else{
+            //     pq.pop();
+            // }
         }
         return total;
+
+        // int total = 0;
+        // for(int i = 0;i<n;i++){
+        //     int currProfit = 0;
+        //     for(int j = 0;j<m;j++){
+        //         if(difficulty[j] <= worker[i]){
+        //             currProfit = max(currProfit, profit[j]);
+        //         }
+        //     }
+        //     total += currProfit;
+        // }
+        // return total;
     }
 };

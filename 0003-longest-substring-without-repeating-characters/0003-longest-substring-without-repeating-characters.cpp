6e0569deadb62319fcaba0f439d2b99b92c0ada7
Rana Dutta <duttarana189@gmail.com>
2025-06-29 23:01:15 +0530

    Time: 11 ms (51.68%), Space: 12.2 MB (42.8%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,11 +6,15 @@ public:
         unordered_map<char, int> mpp;
         int i = 0, j = 0;
         while (j < n) {
-            if (mpp.find(s[j]) != mpp.end()) {
-                i = max(i, mpp[s[j]] + 1); 
+            mpp[s[j]]++;
+            if(mpp.size() == j-i+1){
+                maxLen = max(maxLen, j-i+1);
             }
-            mpp[s[j]] = j;
-            maxLen = max(maxLen, j - i + 1);
+            else if(mpp.size() < j-i+1){
+                mpp[s[i]]--;
+                if(mpp[s[i]] == 0) mpp.erase(s[i]);
+                i++;
+            }
             j++;
         }
         return maxLen;

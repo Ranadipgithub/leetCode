86fe084597ef098a96e2393aae28415d8ddafa3b
Rana Dutta <duttarana189@gmail.com>
2025-06-30 19:57:27 +0530

    Time: 0 ms (100%), Space: 11.4 MB (84.23%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.length();
-        map<char, int> mp;
-        
-        for(char &ch : t) {
-            mp[ch]++;
+        if (s.empty() || t.empty()) {
+            return "";
         }
-        
-        int requiredCount = t.length();
-        int i = 0, j  = 0;
-        int minStart  = 0;
-        int minWindow = INT_MAX;
-        while(j < n) {
-            char ch_j = s[j];
-            if(mp[ch_j] > 0)
-                requiredCount--;
-            
-            mp[ch_j]--;
-            
-            while(requiredCount == 0) {
-                if(minWindow > j-i+1) {
-                    minWindow = j-i+1;
-                    minStart  = i;
-                }
-                
-                char ch_i = s[i];
-                mp[ch_i]++;
-                if(mp[ch_i] > 0)
-                    requiredCount++;
-                i++;
+        vector<int>hash(256,0);
+
+        int l=0,r=0,minlen=INT_MAX,sind=-1,cnt=0;
+
+        int n=s.size(),m=t.size();
+
+        for(int i=0;i<m;i++){
+            hash[t[i]]++;
+        }
+
+        while(r<n){
+
+            if(hash[s[r]]>0){
+               cnt++;
             }
+            hash[s[r]]--;
             
-            j++;
+            while(cnt==m){
+
+                if(r-l+1<minlen){
+                    minlen=r-l+1;
+                    sind=l;
+                }
+                hash[s[l]]++;
+
+                if(hash[s[l]]>0){
+                    cnt=cnt-1;
+                }
+
+                l++;
+            }
+            r=r+1;
         }
         
-        return minWindow == INT_MAX ? "" : s.substr(minStart, minWindow);
+        return sind==-1?"":s.substr(sind,minlen);
     }
 };

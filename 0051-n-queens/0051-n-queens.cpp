55a18b2b4cc305471da258f106ad0775b5c22817
Rana Dutta <duttarana189@gmail.com>
2025-07-02 11:41:24 +0530

    Time: 1 ms (80.56%), Space: 10.3 MB (55.32%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-    bool notInDiag(int row, int col, vector<string> &board, int n){
-        int i = row, j = col;
-        while(i>-1 && j > -1){
-            if(board[i][j] == 'Q') return false;
-            i--;
-            j--;
-        }
-        i = row, j = col;
-        while(i>-1 && j < n){
-            if(board[i][j] == 'Q') return false;
-            i--;
-            j++;
-        }
-        return true;
-    }
-    void solve(int row, int n, vector<string> &board, vector<vector<string>> &res, vector<int> &column){
-        if(row == n){
+    void solve(int row, int n, vector<string> &board, vector<vector<string>> &res,
+               vector<int> &column, vector<int> &diag1, vector<int> &diag2) {
+        if (row == n) {
             res.push_back(board);
             return;
         }
-        for(int i = 0;i<n;i++){
-            if(column[i] == 0 && notInDiag(row, i, board, n)){
-                column[i] = 1;
-                board[row][i] = 'Q';
-                solve(row+1, n, board, res, column);
-                column[i] = 0;
-                board[row][i] = '.';
+
+        for (int col = 0; col < n; col++) {
+            if (column[col] == 0 && diag1[row - col + n - 1] == 0 && diag2[row + col] == 0) {
+                column[col] = 1;
+                diag1[row - col + n - 1] = 1;
+                diag2[row + col] = 1;
+                board[row][col] = 'Q';
+
+                solve(row + 1, n, board, res, column, diag1, diag2);
+
+                column[col] = 0;
+                diag1[row - col + n - 1] = 0;
+                diag2[row + col] = 0;
+                board[row][col] = '.';
             }
         }
     }
+
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> res;
         vector<string> board(n, string(n, '.'));
         vector<int> column(n, 0);
-        solve(0, n, board, res, column);
+        vector<int> diag1(2 * n - 1, 0);
+        vector<int> diag2(2 * n - 1, 0);
+        solve(0, n, board, res, column, diag1, diag2);
         return res;
     }
 };

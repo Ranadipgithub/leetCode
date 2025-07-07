fc74eb7f32428a5da0ce276c847ba793d65c2b53
Rana Dutta <duttarana189@gmail.com>
2025-07-07 20:16:14 +0530

    Time: 0 ms (100%), Space: 21.9 MB (75.48%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,42 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void inOrder(TreeNode *root, vector<int>&res){
-        if(root == nullptr) return;
+    bool inorder(TreeNode* root, long long& prev) {
+        if (!root) return true;
+        if (!inorder(root->left, prev)) return false;
+        if (root->val <= prev) return false;
+        prev = root->val;
+        return inorder(root->right, prev);
+    }
 
-        inOrder(root->left, res);
-        res.push_back(root->val);
-        inOrder(root->right, res);
-    }
-    TreeNode *prev = nullptr;
-    bool inorder(TreeNode *root){
-        if(!root) return true;
-        if(!inorder(root->left)) return false;
-        if(prev && root->val <= prev->val) return false;
-        prev = root;
-        return inorder(root->right);
-    }
     bool isValidBST(TreeNode* root) {
-        return inorder(root);
-        // vector<int> res;
-        // inOrder(root, res);
-        // bool fg = true;
-        // for(int i = 1;i<res.size();i++){
-        //     if(res[i-1]>=res[i]){
-        //         fg = false;
-        //         break;
-        //     }
-        // }
-        // return fg;
+        long long prev = LLONG_MIN; 
+        return inorder(root, prev);
     }
 };

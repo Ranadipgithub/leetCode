8192c10807d6a26a994cb4498024cb793e772a85
Rana Dutta <duttarana189@gmail.com>
2025-07-07 20:39:54 +0530

    Time: 3 ms (19.35%), Space: 16.8 MB (13.36%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,36 +11,23 @@
  */
 class Solution {
 public:
-    // TreeNode* solve(vector<int>& preorder, int start, int end) {
-    //     if (start > end) return nullptr;
-
-    //     TreeNode* root = new TreeNode(preorder[start]);
-
-    //     int rightSubtreeStart = end + 1;
-    //     for (int i = start + 1; i <= end; i++) {
-    //         if (preorder[i] > preorder[start]) {
-    //             rightSubtreeStart = i;
-    //             break;
-    //         }
-    //     }
-
-    //     root->left = solve(preorder, start + 1, rightSubtreeStart - 1);
-    //     root->right = solve(preorder, rightSubtreeStart, end);
-    //     return root;
-    // }
-    TreeNode *solve(vector<int>&preorder, int lower, int upper, int &index){
-        if(index == preorder.size() || preorder[index] < lower || preorder[index] > upper){
-            return nullptr;
-        }
-        TreeNode *root = new TreeNode(preorder[index++]);
-        root->left = solve(preorder, lower, root->val, index);
-        root->right = solve(preorder, root->val, upper, index);
+    TreeNode *solve(vector<int> &pre, int preStart, int preEnd, vector<int>&in, int inStart, int inEnd, unordered_map<int, int> &mpp){
+        if(preStart > preEnd || inStart > inEnd) return NULL;
+        TreeNode *root = new TreeNode(pre[preStart]);
+        int inRoot = mpp[root->val];
+        int numsLeft = inRoot-inStart;
+        root->left = solve(pre, preStart+1, preStart+numsLeft, in, inStart, inRoot-1, mpp);
+        root->right = solve(pre, preStart+numsLeft+1, preEnd, in, inRoot+1, inEnd, mpp);
         return root;
     }
-
     TreeNode* bstFromPreorder(vector<int>& preorder) {
-        // return solve(preorder, 0, preorder.size() - 1);
-        int idx = 0;
-        return solve(preorder, INT_MIN, INT_MAX, idx);
+        vector<int> pre = preorder;
+        sort(preorder.begin(), preorder.end());
+        vector<int> in = preorder;
+        unordered_map<int, int> mpp;
+        for(int i = 0;i<in.size();i++){
+            mpp[in[i]] = i;
+        }
+        return solve(pre, 0, pre.size()-1, in, 0, in.size()-1, mpp);
     }
 };

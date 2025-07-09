9180846a513c0eb89143dfb0f9ce56ed122e198d
Rana Dutta <duttarana189@gmail.com>
2025-07-10 00:44:49 +0530

    Time: 1 ms (86.86%), Space: 20.2 MB (31.43%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,25 +2,17 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int, vector<int>> adj;
-        int n = prerequisites.size();
-        for(auto &vec: prerequisites){
-            int u = vec[0];
-            int v = vec[1];
-
-            // b ---> a
+        vector<int> indegree(numCourses, 0);
+        for(auto &it:prerequisites ){
+            int u = it[0];
+            int v = it[1];
             adj[v].push_back(u);
+            indegree[u]++;
         }
-
-        vector<int> indegree(numCourses,0);
-        for(int i = 0;i<numCourses;i++){
-            for(auto &v: adj[i]){
-                indegree[v]++;
-            }
-        }
+        
 
         queue<int> q;
         int cnt = 0;
-
         for(int i = 0;i<numCourses;i++){
             if(indegree[i] == 0){
                 q.push(i);
@@ -31,7 +23,6 @@ public:
         while(!q.empty()){
             int u = q.front();
             q.pop();
-
             for(auto &v: adj[u]){
                 indegree[v]--;
                 if(indegree[v] == 0){

e52dd88b1ca3c29afd805a45544a8d3c3f531c23
Rana Dutta <duttarana189@gmail.com>
2025-07-10 12:01:53 +0530

    Time: 3 ms (97.03%), Space: 20.8 MB (75.2%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,66 +1,39 @@
 class Solution {
-public:    
+public:
+    vector<vector<int>> directions{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
-    vector<int>kahnAlgorithm(int n, int m,vector<int>adj[], vector<int>indegree){
-           vector<int>order;
-           queue<int>q;
-        
-           for(int i=0;i<n*m;i++){
-                   if(indegree[i] == 0)q.push(i);
-           }
+    int dfs(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& dp) {
+        if (dp[i][j] != -1) return dp[i][j];
 
-           while(!q.empty()){
-                
-                 int node = q.front();
-                 q.pop();
+        int maxLen = 1;
 
-                 order.push_back(node);
+        for (auto& dir : directions) {
+            int newi = i + dir[0];
+            int newj = j + dir[1];
 
-                 for(auto neighbor: adj[node]){
-                       indegree[neighbor]--;
-                       if(indegree[neighbor] == 0){
-                            q.push(neighbor);
-                       }
-                 }
-           }
+            if (newi >= 0 && newi < matrix.size() && newj >= 0 && newj < matrix[0].size()
+                && matrix[newi][newj] > matrix[i][j]) {
+                maxLen = max(maxLen, 1 + dfs(matrix, newi, newj, dp));
+            }
+        }
 
-           return order;
-           
+        dp[i][j] = maxLen;
+        return maxLen;
     }
+
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<int>adj[n*m];
-        vector<int>indegree(n*m);
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        int maxCnt = 0;
 
-        vector<int>dir{0,-1,0,1,0};
-
-        for(int i=0;i<n;i++){
-             for(int j=0;j<m;j++){
-                   
-                   for(int k=0;k<4;k++){
-                          int x = i  + dir[k];
-                          int y = j + dir[k+1];
-
-                          if(x >= 0 and y >= 0 and x < n and y < m and matrix[x][y] > matrix[i][j]){
-                                adj[i*m+j].push_back(x*m+y);
-                                indegree[x*m+y]++;
-                          }
-                   }
-             }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                maxCnt = max(maxCnt, dfs(matrix, i, j, dp));
+            }
         }
 
-        vector<int>order = kahnAlgorithm(n,m,adj,indegree);
-
-        vector<int>dp(n*m,1);
-
-        for(auto it: order){
-             for(auto edge: adj[it]){
-                   dp[edge] = max(dp[edge], dp[it] + 1);
-             }
-        }
-
-        return *max_element(dp.begin(), dp.end());
+        return maxCnt;
     }
 };

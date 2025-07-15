f0627862cc5097b32d5b75d49b9cb23a2c39ba1f
Rana Dutta <duttarana189@gmail.com>
2025-07-15 22:36:25 +0530

    Time: 3 ms (73.58%), Space: 16.6 MB (5.19%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,16 +1,83 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        long long n = nums.size();
-        long long cnt = 0;
-        for(long long i = 0;i<n;i++){
-            long long mini = nums[i], maxi = nums[i];
-            for(long long j = i;j<n;j++){
-                if(nums[j] < mini) mini = nums[j];
-                if(nums[j] > maxi) maxi = nums[j];
-                cnt += (maxi - mini);
+    long long sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<pair<int, int>> prevSmall(n);
+        stack<pair<int, int>> st;
+        
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && st.top().first >= arr[i]){
+                st.pop();
             }
+            prevSmall[i] = st.empty() ? make_pair(-1, -1) : st.top();
+            st.push({arr[i], i});
         }
+
+        st = stack<pair<int, int>>();
+
+        vector<pair<int, int>> nextSmall(n);
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && st.top().first > arr[i]){ 
+                st.pop();
+            }
+            nextSmall[i] = st.empty() ? make_pair(-1, n) : st.top();
+            st.push({arr[i], i});
+        }
+
+        long long cnt = 0;
+        const int MOD = 1e9 + 7;
+
+        for(int i = 0; i < n; i++){
+            int nextSmallerIndex = nextSmall[i].second;
+            int prevSmallerIndex = prevSmall[i].second;
+            long long left = i - prevSmallerIndex;
+            long long right = nextSmallerIndex - i;
+            cnt = (cnt + arr[i] * left* right);
+        }
+
         return cnt;
+    }
+
+    long long sumSubarrayMaxs(vector<int>& arr) {
+        int n = arr.size();
+        vector<pair<int, int>> prevSmall(n);
+        stack<pair<int, int>> st;
+        
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && st.top().first <= arr[i]){
+                st.pop();
+            }
+            prevSmall[i] = st.empty() ? make_pair(-1, -1) : st.top();
+            st.push({arr[i], i});
+        }
+
+        st = stack<pair<int, int>>();
+
+        vector<pair<int, int>> nextSmall(n);
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && st.top().first < arr[i]){ 
+                st.pop();
+            }
+            nextSmall[i] = st.empty() ? make_pair(-1, n) : st.top();
+            st.push({arr[i], i});
+        }
+
+        long long cnt = 0;
+        const int MOD = 1e9 + 7;
+
+        for(int i = 0; i < n; i++){
+            int nextSmallerIndex = nextSmall[i].second;
+            int prevSmallerIndex = prevSmall[i].second;
+            long long left = i - prevSmallerIndex;
+            long long right = nextSmallerIndex - i;
+            cnt = (cnt + arr[i] * left* right);
+        }
+
+        return cnt;
+    }
+
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        return abs(sumSubarrayMins(nums) - sumSubarrayMaxs(nums));
     }
 };

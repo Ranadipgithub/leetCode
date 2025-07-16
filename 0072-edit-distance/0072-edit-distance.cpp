b10cbb189c067538aea54b3b0622d68213183019
Rana Dutta <duttarana189@gmail.com>
2025-07-16 23:24:21 +0530

    Time: 24 ms (18.13%), Space: 14.3 MB (20.82%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     int t[501][501];
-    int solve(string &word1, string &word2, int i, int j){
-        if(i == word1.size()) return word2.size() - j;
-        else if(j == word2.size()) return word1.size() - i;
-        if(t[i][j] != -1) return t[i][j];
-        if(word1[i] == word2[j]) return t[i][j] = solve(word1, word2, i+1, j+1);
+
+    int solve(string &word1, string &word2, int n, int m) {
+        if(m == 0) return n;
+        else if(n == 0) return m;
+        if(t[n][m] != -1) return t[n][m];
+
+        if(word1[n-1] == word2[m-1]) 
+            return t[n][m] = solve(word1, word2, n-1, m-1);
         else {
-            int insert = 1 + solve(word1, word2, i, j+1);
-            int deleteOp = 1+  solve(word1, word2, i+1, j);
-            int replace = 1 + solve(word1, word2, i+1, j+1);
-            return t[i][j] = min({insert, deleteOp, replace});
+            int insert = 1 + solve(word1, word2, n, m-1);
+            int deleteOp = 1 + solve(word1, word2, n-1, m);
+            int replace = 1 + solve(word1, word2, n-1, m-1);
+            return t[n][m] = min({insert, deleteOp, replace});
         }
     }
+
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        memset(t, -1, sizeof(t));
+        
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-        return solve(word1, word2, 0, 0);
+        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min({dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]});
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };

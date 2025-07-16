443d9a2674af51cf444868d741806fb189d6b8d6
Rana Dutta <duttarana189@gmail.com>
2025-07-17 02:20:22 +0530

    Time: 275 ms (19.92%), Space: 291.7 MB (13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,35 +2,28 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        if (n < 2) return s;
-        string s1 = s;
-        reverse(s.begin(), s.end());
-        string s2 = s;
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        int maxEl = 0;
-        int maxi = 0, maxj = 0;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s1[i-1] == s2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                    int len = dp[i][j];
-                    if (len > maxEl && (i - len) == (n - j)) {
-                        maxEl = len;
-                        maxi = i;
-                        maxj = j;
-                    }
-                } else {
-                    dp[i][j] = 0;
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        int maxLen = 1;
+        int start = 0;
+
+        for (int l = 1; l <= n; l++) {
+            for (int i = 0; i + l - 1 < n; i++) {
+                int j = i + l - 1;
+                if (l == 1) {
+                    dp[i][j] = 1;
+                } else if (l == 2 && s[i] == s[j]) {
+                    dp[i][j] = 1;
+                } else if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = 1;
+                }
+
+                if (dp[i][j] && l > maxLen) {
+                    maxLen = l;
+                    start = i;
                 }
             }
         }
-        string res = "";
-        int i = maxi, j = maxj;
-        while (i > 0 && j > 0 && dp[i][j] > 0) {
-            res += s1[i-1];
-            i--; 
-            j--;
-        }
-        return res;
+
+        return s.substr(start, maxLen);
     }
 };

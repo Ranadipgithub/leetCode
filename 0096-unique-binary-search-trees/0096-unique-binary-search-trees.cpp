eafd5edaf9ffd07594785bd7d2081aa89fb8a74c
Rana Dutta <duttarana189@gmail.com>
2025-07-18 15:15:23 +0530

    Time: 2215 ms (5.02%), Space: 7.9 MB (78.78%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -12,8 +12,24 @@ public:
         }
         return dp[n] = cnt;
     }
+
+    int findCatalan(int n) {
+    
+    // Base case
+        if (n <= 1)
+            return 1;
+
+        // catalan(n) is sum of
+        // catalan(i)*catalan(n-i-1)
+        int res = 0;
+        for (int i = 0; i < n; i++)
+            res += findCatalan(i) * findCatalan(n - i - 1);
+
+        return res;
+    }
+
     int numTrees(int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(n);
+        // memset(dp, -1, sizeof(dp));
+        return findCatalan(n);
     }
 };

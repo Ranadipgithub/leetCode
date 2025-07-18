fa632d008e5c079fbba2d004c56249cae763904d
Rana Dutta <duttarana189@gmail.com>
2025-07-18 15:03:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (89.53%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public: 
-    
+    int dp[20];
     int solve(int n){
         if(n == 0 || n == 1) return 1;
         int cnt = 0;
+        if(dp[n] != -1) return dp[n];
         for(int i = 1;i<=n;i++){
             int numsLeft = i-1;
             int numsRight = n-i;
             cnt += solve(numsLeft) * solve(numsRight);
         }
-        return cnt;
+        return dp[n] = cnt;
     }
     int numTrees(int n) {
+        memset(dp, -1, sizeof(dp));
         return solve(n);
     }
 };

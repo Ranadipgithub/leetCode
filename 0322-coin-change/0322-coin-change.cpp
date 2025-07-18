15484c26522836611a6b7f96fc4ba7884ba245e1
Rana Dutta <duttarana189@gmail.com>
2025-07-18 19:24:18 +0530

    Time: 58 ms (16.27%), Space: 49.8 MB (20.5%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    // vector<vector<int>>dp;
-    // int solve(vector<int>&coins, int amt, int n){
-    //     if(amt == 0) return 0;
-    //     if(n == 0 || amt < 0) return 1e9;
-    //     if(dp[n][amt] != -1) return dp[n][amt];
-    //     int take = 1e9;
-    //     if(coins[n-1] <= amt){
-    //         take = 1+solve(coins, amt-coins[n-1], n);
-    //     }
-    //     int skip = solve(coins, amt, n-1);
-    //     return dp[n][amt] = min(take, skip);
-    // }
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        // dp.assign(n+1, vector<int>(amount+1, -1));
-        // int ans = solve(coins, amount, n);
-        // return ans == 1e9? -1: ans;
+    vector<vector<int>> dp;
+    int solve(vector<int>& coins, int amt, int idx) {
+        if (amt == 0) return 0;
+        if (idx >= coins.size()) return INT_MAX - 1;
 
-        vector<vector<int>>dp(n+1, vector<int>(amount+1, 1e9));
-        for(int i = 0;i<=n;i++) dp[i][0] = 0;
-        for(int i = 1;i<=n;i++){
-            for(int j = 1;j<=amount;j++){
-                if(coins[i-1] <= j){
-                    dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
-                } else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
+        if(dp[idx][amt] != -1) return dp[idx][amt]; 
+
+        int take = INT_MAX;
+        if (amt - coins[idx] >= 0) {
+            int res = solve(coins, amt - coins[idx], idx);
+            if (res != INT_MAX - 1) take = 1 + res;
         }
-        return dp[n][amount] == 1e9? -1: dp[n][amount];
+
+        int not_take = solve(coins, amt, idx + 1);
+
+        return dp[idx][amt] = min(take, not_take);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        dp.resize(coins.size(), vector<int>(amount + 1, -1));
+        int res = solve(coins, amount, 0);
+        return (res == INT_MAX - 1) ? -1 : res;
     }
 };

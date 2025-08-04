234a0781d539378d030fe1695646945e22ef4cd0
Rana Dutta <duttarana189@gmail.com>
2025-08-04 09:37:09 +0530

    Time: 0 ms (100%), Space: 21.8 MB (44.25%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(), intervals.end());
         int n = intervals.size();
         vector<vector<int>> res;
-        for(int i = 0;i<n;i++){
-            if(res.empty() || intervals[i][0] > res.back()[1]){
+        bool inserted = false; 
+
+        for(int i = 0; i < n; i++) {
+            if (intervals[i][1] < newInterval[0]) {
+                // Current interval ends before newInterval starts
                 res.push_back(intervals[i]);
-            } else res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else if (intervals[i][0] > newInterval[1]) {
+                // Current interval starts after newInterval ends
+                if (!inserted) {
+                    res.push_back(newInterval);
+                    inserted = true;
+                }
+                res.push_back(intervals[i]);
+            } else {
+                // Overlapping intervals
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
         }
+
+        if (!inserted) {
+            res.push_back(newInterval);
+        }
+
         return res;
     }
 };

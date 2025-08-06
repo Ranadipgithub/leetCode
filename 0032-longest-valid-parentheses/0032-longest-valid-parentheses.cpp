e366568bc2d9fbabcba0dc0c146b7f4a6ed0d40c
Rana Dutta <duttarana189@gmail.com>
2025-08-06 19:18:02 +0530

    Time: 21 ms (5.01%), Space: 15.7 MB (5.29%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,34 +2,51 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         int n = s.size();
-        int left = 0, right = 0, maxLen = 0;
+        int len = 0, maxLen = 0;
+        stack<int> st;
         for(int i = 0;i<n;i++){
             if(s[i] == '('){
-                left++;
-
-            } 
-            else right++;
-            if(left == right){
-                maxLen = max(maxLen, left*2);
-            } else if(left < right){
-                left = 0;
-                right = 0;
+                st.push(i);
+            } else{
+                if(!st.empty() && s[st.top()] == '('){
+                    st.pop();
+                } else {
+                    st.push(i);
+                }
             }
         }
-        left = 0, right = 0;
-        for(int i = n-1;i>=0;i--){
-            if(s[i] == ')'){
-                right++;
 
-            } 
-            else left++;
-            if(left == right){
-                maxLen = max(maxLen, left*2);
-            } else if(left > right){
-                left = 0;
-                right = 0;
+        set<int> idx;
+
+        while(!st.empty()){
+            idx.insert(st.top());
+            st.pop();
+        }
+
+        for(int i = 0;i<n;i++){
+            if(idx.find(i) != idx.end()){
+                len = 0;
+            }
+            else {
+                len++;
+                maxLen = max(maxLen, len);
             }
         }
+
+
+
+        // for(int i = 0;i<n;i++){
+        //     if(s[i] == '('){
+        //         st.push(s[i]);
+        //     } else{
+        //         if(st.empty()) len = 0;
+        //         else {
+        //             st.pop();
+        //             len += 2;
+        //             maxLen = max(maxLen, len);
+        //         }
+        //     }
+        // }
         return maxLen;
     }
 };

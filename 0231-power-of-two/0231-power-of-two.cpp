40f07f19bffdf9b8aa56e55e064c25857080edf8
Rana Dutta <duttarana189@gmail.com>
2025-08-09 08:44:23 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.2%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0)
-            return false;
-        if(n == 1)
-            return true;
-        
-        return __builtin_popcount(n) == 1;
+        if(n == 0) return false;
+        long long num = (long long)n;
+        return (num&(num-1)) == 0;
     }
 };

9faf7ee71ef2852976a7b9931452074f62ebd6b0
Rana Dutta <duttarana189@gmail.com>
2025-08-11 10:03:17 +0530

    Time: 37 ms (42.02%), Space: 148 MB (37.23%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     const int MOD = 1e9+7;
+
+    long long power(long long base, long long exp) {
+        long long res = 1;
+        base %= MOD;
+        while (exp > 0) {
+            if (exp % 2 == 1) res = (res * base) % MOD;
+            base = (base * base) % MOD;
+            exp /= 2;
+        }
+        return res;
+    }
+
+    long long modInverse(long long n) {
+        return power(n, MOD - 2);
+    }
+
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
         string s = "";
         while(n != 1){
@@ -18,15 +34,25 @@ public:
                 pdt.push_back(1<<idx);
             }
         }
+        
+        int n1 = pdt.size();
+        vector<long long> pre(n1);
+        pre[0] = pdt[0];
+        for(int i = 1; i < pdt.size(); i++){
+            pre[i] = (pdt[i] * pre[i-1]) % MOD;
+        }
+        
         vector<int> res;
-        for(auto it: queries){
+        for(auto& it: queries){
             int left = it[0];
             int right = it[1];
-            long long pdts = 1;
-            for(int i = left;i<=right;i++){
-                pdts = (pdts * pdt[i])%MOD;
+            if(left == 0) {
+                res.push_back((int)pre[right]);
+            } else {
+                long long left_inv = modInverse(pre[left-1]);
+                long long ans = (pre[right] * left_inv) % MOD;
+                res.push_back((int)ans);
             }
-            res.push_back((int)pdts);
         }
         return res;
     }

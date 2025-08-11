a2793cd965fbce0f748d31825584f2ca64e17c83
Rana Dutta <duttarana189@gmail.com>
2025-08-11 21:07:32 +0530

    Time: 16 ms (26.9%), Space: 12.3 MB (24.5%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,50 +1,25 @@
 class Solution {
-private:
-    // // vector<vector<int>> dp; 
-
-    // int solve(int n, int currA, int clip, int count) {
-    //     if (currA > n) return INT_MAX;
-    //     if (currA == n) return count;  
-        
-    //     if (dp[currA][clip] != -1) return dp[currA][clip];
-
-    //     int copyAndPaste = INT_MAX, paste = INT_MAX;
-        
-    //     if (clip != currA) {
-    //         copyAndPaste = solve(n, currA * 2, currA, count + 2);
-    //     }
-
-    //     if (clip > 0) {
-    //         paste = solve(n, currA + clip, clip, count + 1);
-    //     }
-
-    //     return dp[currA][clip] = min(copyAndPaste, paste);
-    // }
-
 public:
+    int dp[1001][1001];
+    int solve(int currLen, int clipboard, int target) {
+        if (currLen > target) return 1e9; 
+        if (currLen == target) return 0; 
+        
+        if (dp[currLen][clipboard] != -1) 
+            return dp[currLen][clipboard];
+
+
+        int copy = 1e9;
+        if (clipboard != currLen)
+            copy = 1 + solve(currLen, currLen, target);
+
+        int paste = (clipboard > 0) ? 1 + solve(currLen + clipboard, clipboard, target) : 1e9;
+
+        return dp[currLen][clipboard] = min(copy, paste);
+    }
+
     int minSteps(int n) {
-        // dp.assign(n + 1, vector<int>(n + 1, -1)); 
-        // return solve(n, 1, 0, 0); 
-        if(n == 0) return 0;
-        if(n == 1) return 0;
-        vector<int> dp(n+1, 0);
-        dp[0] = 0;
-        dp[1] = 0;
-        dp[2] = 2;
-        for(int i = 3;i<=n;i++){
-            int factor = i/2;
-            while(factor > 0){
-                if(i % factor == 0){
-                int steps = dp[factor];
-                int copy = 1;
-                int paste = (i / factor) - 1;
-                dp[i] = steps + copy + paste;
-                break;
-                }
-                else factor--;
-            }
-            
-        }
-        return dp[n];
+        memset(dp, -1, sizeof(dp));
+        return solve(1, 0, n);
     }
 };

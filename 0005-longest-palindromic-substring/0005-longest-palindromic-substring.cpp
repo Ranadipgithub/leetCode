0f987dd709ac93f0380a0a080d13af25aa2c8e92
Rana Dutta <duttarana189@gmail.com>
2025-08-12 11:02:52 +0530

    Time: 202 ms (23.24%), Space: 291.9 MB (10.98%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -5,25 +5,27 @@ public:
         vector<vector<int>> dp(n, vector<int>(n, 0));
         int maxLen = 1;
         int start = 0;
-
+        
         for (int l = 1; l <= n; l++) {
             for (int i = 0; i + l - 1 < n; i++) {
                 int j = i + l - 1;
                 if (l == 1) {
                     dp[i][j] = 1;
+                    maxLen = 1;
+                    start = i;
                 } else if (l == 2 && s[i] == s[j]) {
                     dp[i][j] = 1;
+                    maxLen = 2;
+                    start = i;
                 } else if (s[i] == s[j] && dp[i + 1][j - 1]) {
                     dp[i][j] = 1;
-                }
-
-                if (dp[i][j] && l > maxLen) {
-                    maxLen = l;
-                    start = i;
+                    if(j-i+1 > maxLen){
+                        maxLen = j-i+1;
+                        start = i;
+                    }
                 }
             }
         }
-
         return s.substr(start, maxLen);
     }
 };

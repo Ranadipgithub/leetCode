104c31b1825af48170b0c5fce84c747a6949e4fe
Rana Dutta <duttarana189@gmail.com>
2025-08-15 18:12:04 +0530

    Time: 172 ms (18.74%), Space: 19.4 MB (42.88%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            string str = "";
+            vector<int> temp(26, 0);
+            for (int j = i; j < n; j++) {
+                str += s[j];
+                temp[s[j] - 'a']++;
+                int maxi = *max_element(temp.begin(), temp.end());
+                int mini = INT_MAX;
+                for (int k = 0; k < 26; k++) {
+                    if (temp[k] > 0) mini = min(mini, temp[k]);
+                }
+                ans += maxi - mini;
+            }
+        }
+        return ans;
+    }
+};

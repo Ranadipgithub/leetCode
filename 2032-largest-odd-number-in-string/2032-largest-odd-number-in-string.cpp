1ff51558b80ba356b22af0fbadcaeae95bd3a930
Rana Dutta <duttarana189@gmail.com>
2025-08-15 09:53:43 +0530

    Time: 1 ms (42.96%), Space: 17.7 MB (62.98%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     string largestOddNumber(string num) {
         int n = num.size();
-        int lastIdx = -1;
+        // int lastIdx = -1;
         for(int i = n-1;i>=0;i--){
             if((num[i] - '0')%2 == 1){
-                lastIdx =i;
-                break;
+                // lastIdx =i;
+                return num.substr(0, i+1);
+                // break;
             }
         }
-        if(lastIdx == -1) return "";
-        string res = num.substr(0, lastIdx+1);
-        return res;
+        // if(lastIdx == -1) return "";
+        // string res = num.substr(0, lastIdx+1);
+        // return res;
+        return "";
     }
 };

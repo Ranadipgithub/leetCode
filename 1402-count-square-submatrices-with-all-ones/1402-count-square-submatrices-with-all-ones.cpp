b0474288a5003181db4b65c96542bd83bda33686
Rana Dutta <duttarana189@gmail.com>
2025-08-20 10:04:32 +0530

    Time: 11 ms (28.49%), Space: 29 MB (85.25%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& matrix, int i, int j, int newi, int newj) {
-        int sum = 0;
-        for (int k = i; k <= newi; k++) {
-            for (int l = j; l <= newj; l++) {
-                sum += matrix[k][l];
-            }
-        }
-        return sum;
+    int dp[301][301];
+    int solve(vector<vector<int>>& matrix, int i, int j){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        if(i>=n || j >= m || matrix[i][j] == 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int right = solve(matrix, i, j+1);
+        int diag = solve(matrix, i+1, j+1);
+        int bottom = solve(matrix, i+1, j);
+        return dp[i][j] = 1 + min({right, diag, bottom});
     }
-
     int countSquares(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int total = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                for (int k = 0; i + k < n && j + k < m; k++) {
-                    int newi = i + k;
-                    int newj = j + k;
-                    int sum = solve(matrix, i, j, newi, newj);
-                    int temp = (newi - i + 1) * (newj - j + 1);
-                    if (sum == temp) total++;
-                    else break;
-                }
+        memset(dp, -1, sizeof(dp));
+        int cnt = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(matrix[i][j] == 0) continue;
+                cnt += solve(matrix, i, j);
             }
         }
-        return total;
+        return cnt;
     }
 };

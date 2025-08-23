70520be3b3c6ca4db9c88dc55f5489292d0c74dc
Rana Dutta <duttarana189@gmail.com>
2025-08-23 10:57:38 +0530

    Time: 92 ms (48.25%), Space: 141.2 MB (35.79%) - LeetHub

--- 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
+    void dfs(int node, unordered_map<int, vector<pair<int, int>>> &adj, vector<int>& visited, int &mini) {
+        visited[node] = 1;
+        for (auto [v, w] : adj[node]) {
+            mini = min(mini, w); 
+            if (!visited[v]) {
+                dfs(v, adj, visited, mini);
+            }
+        }
+    }
+
     int minScore(int n, vector<vector<int>>& roads) {
         unordered_map<int, vector<pair<int, int>>> adj;
-        for (auto& it : roads) {
+        for (auto &it : roads) {
             int u = it[0], v = it[1], w = it[2];
             adj[u].push_back({v, w});
             adj[v].push_back({u, w});
         }
-
-        queue<int> q;
-        vector<int> visited(n + 1, 0);
+        vector<int> visited(n+1, 0);
         int mini = INT_MAX;
-        q.push(1);
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            if (visited[node]) continue;
-            visited[node] = 1;
-            for (auto& [v, w] : adj[node]) {
-                mini = min(mini, w);
-                if (!visited[v]) {
-                    q.push(v);
-                }
-            }
-        }
+        dfs(1, adj, visited, mini); 
         return mini;
     }
 };

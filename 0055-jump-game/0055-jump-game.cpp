8883b82232990cdbd1c3224a11fbe9e5e35c4383
Rana Dutta <duttarana189@gmail.com>
2025-08-24 11:49:11 +0530

    Time: 593 ms (8.78%), Space: 55.6 MB (7.08%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dp;
+    bool solve(vector<int>& nums, int idx){
+        if(idx >= nums.size() - 1) return true;
+        if(dp[idx] != -1) return dp[idx];
+        for(int i = 1; i <= nums[idx]; i++){
+            if(solve(nums, idx + i)) return dp[idx] = true;
+        }
+        return dp[idx] = false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        dp.resize(n+1, -1);
+        return solve(nums, 0);
+    }
+};

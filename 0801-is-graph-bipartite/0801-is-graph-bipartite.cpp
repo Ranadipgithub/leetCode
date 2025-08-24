2abdefd53628e79423eb2d6ac195fb4f548d4421
Rana Dutta <duttarana189@gmail.com>
2025-08-24 21:19:26 +0530

    Time: 0 ms (100%), Space: 17.4 MB (54.65%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph, int node, vector<int>&color, int currColor){
-        color[node] = currColor;
+    bool dfs(vector<vector<int>>& graph, int node, vector<int>& visited, int parent, vector<int>& count){
+        visited[node] = 1;
         for(int &v: graph[node]){
-            if(color[v] == color[node]){
-                return false;
-            }
-            if(color[v] == -1){
-                int colorV = 1 - color[node];
-                if(dfs(graph, v, color, colorV) == false) return false;
-            }
-        }
-        return true;
-    }
-    bool bfs(vector<vector<int>>& graph, int node, vector<int>&color, int currColor){
-        queue<int> q;
-        q.push(node);
-        color[node] = currColor;
-        while(!q.empty()){
-            int u = q.front();
-            q.pop();
-            for(int &v: graph[u]){
-                if(color[v] == color[u]){
-                    return false;
-                }
-                if(color[v] == -1){
-                    color[v] = 1-color[u];
-                    q.push(v);
-                }
+            if(v == parent) continue;  
+
+            if(!visited[v]){
+                count[v] = count[node] + 1; 
+                if(!dfs(graph, v, visited, node, count)) return false;
+            } else {
+                int len = count[node] - count[v] + 1;
+                if(len % 2 == 1) return false;  
             }
         }
         return true;
     }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> color(n,-1);
-        for(int i = 0;i<n;i++){
-            if(color[i] == -1){
-                if(!bfs(graph, i, color, 1)){
-                    return false;
-                }
+        vector<int> visited(n, 0);
+        vector<int> count(n, 0);
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                count[i] = 0; 
+                if(!dfs(graph, i, visited, -1, count)) return false;
             }
         }
         return true;

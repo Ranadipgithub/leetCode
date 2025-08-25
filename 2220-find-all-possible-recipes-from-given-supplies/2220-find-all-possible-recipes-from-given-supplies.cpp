dc80f9165b8083677f841ba16cdf4b43395643df
Rana Dutta <duttarana189@gmail.com>
2025-08-25 11:44:46 +0530

    Time: 48 ms (81.95%), Space: 101.2 MB (84.48%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_map<string, vector<int>> adj;
         unordered_set<string> st(supplies.begin(), supplies.end());
         int n = recipes.size();
-        vector<string> res;
-        vector<int> done(n, 0); 
-
-        while(n--){ 
-            for(int i = 0;i<recipes.size();i++){
-                if(done[i]) continue;
-                bool fg = true;
-                for(int j = 0;j<ingredients[i].size();j++){
-                    if(!st.count(ingredients[i][j])){
-                        fg = false;
-                        break;
-                    }
+        vector<int> indegree(n, 0);
+        for(int i = 0;i<n;i++){
+            for(string s: ingredients[i]){
+                if(!st.count(s)){
+                    adj[s].push_back(i);
+                    indegree[i]++;
                 }
-                if(fg){
-                    res.push_back(recipes[i]);
-                    st.insert(recipes[i]);
-                    done[i] = 1;
+            }
+        }
+        queue<int> q;
+        for(int i = 0;i<n;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        vector<string> res;
+        while(!q.empty()){
+            int i = q.front();
+            q.pop();
+            string recipe = recipes[i];
+            res.push_back(recipe);
+
+            for(int &idx: adj[recipe]){
+                indegree[idx]--;
+                if(indegree[idx] == 0){
+                    q.push(idx);
                 }
             }
         }

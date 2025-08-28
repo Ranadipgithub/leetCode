1d41b025b43ac88ae0cbe3b1d01fbef6509de601
Rana Dutta <duttarana189@gmail.com>
2025-08-28 09:57:23 +0530

    Time: 59 ms (46.06%), Space: 24.8 MB (39.06%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    typedef pair<int, pair<int, int>>P;
-    vector<vector<int>> direction{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    vector<vector<int>> directions{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int n= heights.size();
+        int n = heights.size();
         int m = heights[0].size();
-        priority_queue<P, vector<P>, greater<P>>pq;
-        pq.push({0, {0,0}});
-        vector<vector<int>> res(n, vector<int>(m, INT_MAX));
-        res[0][0] = 0;
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        pq.push({0, {0, 0}});
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        dist[0][0] = 0;
         while(!pq.empty()){
-            auto coord = pq.top().second;
-            int diff = pq.top().first;
+            pair<int,int> node = pq.top().second;
+            int wt = pq.top().first;
+            int x = node.first, y = node.second;
             pq.pop();
-            int x = coord.first;
-            int y = coord.second;
 
-            if(x == n-1 && y == m-1) return diff;
-
-            for(auto dir: direction){
+            for(auto &dir: directions){
                 int newx = x + dir[0];
                 int newy = y + dir[1];
-
                 if(newx < 0 || newx >= n || newy < 0 || newy >= m) continue;
-                int absdiff = abs(heights[newx][newy] - heights[x][y]);
-                int maxdiff = max(diff, absdiff);
 
-                if(res[newx][newy] > maxdiff){
-                    res[newx][newy] = maxdiff;
-                    pq.push({maxdiff, {newx, newy}});
+                int cost = max(wt, abs(heights[newx][newy] - heights[x][y]));
+                if(dist[newx][newy] > cost){
+                    dist[newx][newy] = cost;
+                    pq.push({cost, {newx, newy}});
                 }
-
             }
         }
-        return res[n-1][m-1];
+        return dist[n-1][m-1];
     }
 };

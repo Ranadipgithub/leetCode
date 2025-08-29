846c9b9f9c84e2d327b118843d733d0be5ec0f1b
Rana Dutta <duttarana189@gmail.com>
2025-08-29 09:09:08 +0530

    Time: 25 ms (72.44%), Space: 18.6 MB (42.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,27 +1,58 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int i, int j, int n, int m){
-        if(i < 0 || i >= n|| j<0 || j>= m || grid[i][j] != '1'){
-            return;
+    vector<int> parent;
+    vector<int> rank;
+    vector<vector<int>> directions{{-1, 0}, {0, -1}, {1,0}, {0, 1}};
+    int find(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void unionSet(int x, int y){
+        int parent_x = find(x);
+        int parent_y = find(y);
+
+        if(parent_x == parent_y) return;
+
+        if(rank[parent_x] > rank[parent_y]){
+            parent[parent_y] = parent_x;
+        } else if(rank[parent_y] > rank[parent_x]){
+            parent[parent_x] = parent_y;
+        } else {
+            parent[parent_y] = parent_x;
+            rank[parent_x]++;
         }
-        grid[i][j] = '$';
-        dfs(grid, i+1, j, n, m);
-        dfs(grid, i-1, j, n, m);
-        dfs(grid, i, j+1, n, m);
-        dfs(grid, i, j-1, n, m);
     }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int cnt = 0;
+        rank.resize(n*m, 0);
+        parent.resize(n*m);
+        for(int i = 0;i<n*m;i++){
+            parent[i] = i;
+        }
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid, i, j, n, m);
-                    cnt++;
+                    for(auto &dir: directions){
+                        int newi = i + dir[0];
+                        int newj = j + dir[1];
+
+                        if(newi >= 0 && newi < n && newj >= 0 && newj < m && grid[newi][newj] == '1'){
+                            unionSet(i*m+j, newi*m+newj);
+                        }
+                    }
                 }
             }
         }
-        return cnt;
+        unordered_set<int> st;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j] == '1'){
+                    st.insert(find(i*m+j));
+                }
+            }
+        }
+        return st.size();
     }
 };

f296e7559daf3f8d679abc6152803b5db4531119
Rana Dutta <duttarana189@gmail.com>
2025-10-03 11:44:39 +0530

    Time: 692 ms (57.23%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,8 +1,16 @@
 # Write your MySQL query statement below
-with cte as (
-select d.name as Department, e.Name as Employee, e.salary as Salary, MAX(e.salary) OVER(PARTITION BY e.departmentId) as max_salary from Employee e
-LEFT JOIN Department d
-on e.departmentId=d.id)
+-- with cte as (
+-- select d.name as Department, e.Name as Employee, e.salary as Salary, MAX(e.salary) OVER(PARTITION BY e.departmentId) as max_salary from Employee e
+-- LEFT JOIN Department d
+-- on e.departmentId=d.id)
 
-Select Department, Employee, Salary from cte
-where Salary = max_salary
+-- Select Department, Employee, Salary from cte
+-- where Salary = max_salary
+
+with cte as
+(select d.name as Department, e.name as Employee, e.salary as salary, max(e.salary) over(partition by e.departmentId) as max_salary from Employee e
+Left Join department d
+on e.departmentId = d.id)
+
+select Department, Employee, Salary from cte
+where salary = max_salary;

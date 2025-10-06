c5e0c3568eb4db6a5ffc457fbd092eba88de21fe
Rana Dutta <duttarana189@gmail.com>
2025-10-06 16:59:06 +0530

    Time: 5 ms (59.71%), Space: 12.4 MB (64.22%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -11,15 +11,14 @@ public:
         mpp['D'] = 500;
         mpp['M'] = 1000;
         int no = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (i > 0 && mpp[s[i]] > mpp[s[i - 1]]) {  
-                no += mpp[s[i]] - mpp[s[i - 1]];
-                i--;
+        for(int i = 0;i<n;i++){
+            if(i<n-1 && mpp[s[i]] < mpp[s[i+1]]){
+                no += mpp[s[i+1]] - mpp[s[i]];
+                i++;
             } else {
                 no += mpp[s[i]];
             }
         }
-
         return no;
     }
 };

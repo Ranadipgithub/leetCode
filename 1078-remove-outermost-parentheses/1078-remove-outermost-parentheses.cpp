f4c1e2cb943f5545fecb6059cac566536df904a2
Rana Dutta <duttarana189@gmail.com>
2025-10-06 12:32:45 +0530

    Time: 3 ms (18.57%), Space: 11.8 MB (7.22%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -3,32 +3,30 @@ public:
     string removeOuterParentheses(string s) {
         int n = s.size();
         stack<char> st;
-        vector<string> res;
         string temp;
+        vector<string>res;
         for(int i = 0;i<n;i++){
             if(s[i] == '('){
                 if(!st.empty()){
                     temp += '(';
                     st.push('(');
                 } else{
-                    temp = "(";
+                    temp = "";
                     st.push('(');
                 }
             } else{
-                temp += ')';
                 st.pop();
-
                 if(st.empty()){
                     res.push_back(temp);
-                    temp = "";
+                } else{
+                    temp += ')';
                 }
             }
         }
+
         string ans;
-        for(auto str: res){
-            // for(auto ch: str) cout << ch;
-            // cout << endl;
-            for(int i = 1;i<str.size()-1;i++) ans += str[i];
+        for(int i = 0;i<res.size();i++){
+            ans += res[i];
         }
         return ans;
     }

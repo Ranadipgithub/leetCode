2d34582f67987b75a48e9086ac7fa9611d92d79c
Rana Dutta <duttarana189@gmail.com>
2025-10-14 23:57:09 +0530

    Time: 370 ms (46.7%), Space: 11.9 MB (11.8%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> directions{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-    bool dfs(int i, int j, vector<vector<char>>& board, string word, string temp, int idx, vector<vector<int>>&visited){
+    bool dfs(int i, int j, vector<vector<char>>& board, string word, int idx, vector<vector<int>>&visited){
         int n = board.size(), m = board[0].size();
         if(idx >= word.size()){
             return true;
@@ -11,7 +11,7 @@ public:
             int newj = j + dir[1];
             if(newi >= 0 && newi < n && newj >= 0 && newj < m && !visited[newi][newj] && board[newi][newj] == word[idx]){
                 visited[newi][newj] = 1;
-                if(dfs(newi, newj, board, word, temp, idx+1, visited)) return true;
+                if(dfs(newi, newj, board, word, idx+1, visited)) return true;
                 visited[newi][newj] = 0;
             } 
         }
@@ -24,7 +24,7 @@ public:
                 if(board[i][j] == word[0]){
                     vector<vector<int>> visited(n, vector<int>(m, 0));
                     visited[i][j] = 1;
-                    if(dfs(i, j, board, word, "", 1, visited)) return true;
+                    if(dfs(i, j, board, word, 1, visited)) return true;
                 }
             }
         }

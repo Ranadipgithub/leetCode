0cea21582c06ecb3b18ec5714da4b653ede083b3
Rana Dutta <duttarana189@gmail.com>
2025-10-21 18:47:40 +0530

    Time: 128 ms (60.68%), Space: 111.9 MB (49.69%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,26 +1,62 @@
 class Solution {
-private:
-    void insert(vector<int>& nums, int temp){
-        int n = nums.size();
-        if(n == 0 || nums[n - 1] <= temp){
-            nums.push_back(temp);
-            return;
-        }
-        int val = nums[n - 1];
-        nums.pop_back();
-        insert(nums, temp);
-        nums.push_back(val);
-    }
 public:
+    struct TrieNode {
+        TrieNode *left;
+        TrieNode *right;
+        bool isEnd;
+        int value;
+        int count;
+    };
+
+    TrieNode *getNode() {
+        TrieNode *newNode = new TrieNode();
+        newNode->left = NULL;
+        newNode->right = NULL;
+        newNode->isEnd = false;
+        newNode->value = 0;
+        newNode->count = 0;
+        return newNode;
+    }
+
+    void insert(int num, TrieNode *root) {
+        unsigned int n = static_cast<unsigned int>(num) ^ 0x80000000;
+
+        for (int i = 31; i >= 0; i--) {
+            int bit = (n >> i) & 1;
+            if (bit == 0) {
+                if (!root->left) root->left = getNode();
+                root = root->left;
+            } else {
+                if (!root->right) root->right = getNode();
+                root = root->right;
+            }
+        }
+        root->isEnd = true;
+        root->value = num;
+        root->count += 1;
+    }
+
+    void dfs(TrieNode* node, vector<int>& result) {
+        if (!node) return;
+        if (node->isEnd) {
+            for (int i = 0; i < node->count; i++)
+                result.push_back(node->value);
+        }
+        dfs(node->left, result);
+        dfs(node->right, result);
+    }
+
+    vector<int> getSorted(TrieNode* root) {
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        return nums;
-        // if(n == 1 || n == 0) return nums;
-        // int temp = nums[n-1];
-        // nums.pop_back();
-        // sortArray(nums);
-        // insert(nums, temp);
-        // return nums;
+        TrieNode *root = getNode();
+        for (int num : nums) {
+            insert(num, root);
+        }
+        return getSorted(root);
     }
 };

777475419071c955736bb1fc680d938fab2a1d9b
Rana Dutta <duttarana189@gmail.com>
2025-10-23 10:41:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (18.19%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    vector<int> memo = vector<int>(46, -1);
-    int climbStairs(int n) {
-        // if (n < 0) return 0;
-        // if (n == 0) return 1;
-        // if (memo[n] != -1) return memo[n];
-        
-        // int one = climbStairs(n - 1);
-        // int two = climbStairs(n - 2);
-        // return memo[n] = one + two;
-
-        if( n == 1 || n == 2 || n == 3) return n;
-        vector<int> t(n+1);
-        t[0] = 0;
-        t[1] = 1;
-        t[2] = 2;
-        for(int i = 3;i<=n;i++){
-            t[i] = t[i-1]+t[i-2];
+    vector<int> dp;
+    int solve(int i, int n){
+        if(i == n){
+            return 1;
         }
-        return t[n];
+        if(i>n) return 0;
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = solve(i+1, n) + solve(i+2, n);
+    }
+    int climbStairs(int n) {
+        dp.resize(n, -1);
+        return solve(0, n);
     }
 };

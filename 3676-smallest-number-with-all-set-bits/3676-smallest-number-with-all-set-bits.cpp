92ca5411173069bd9a380e4cdd87b00563de6ffd
Rana Dutta <duttarana189@gmail.com>
2025-10-29 12:06:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (11.95%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        int idx = 0;
-        for (int i = 0; i < 31; i++) {
-            if ((n & (1 << i)) != 0) {
-                idx = i;
-            }
-        }
-        
-        int sum = 0;
-        for (int i = 0; i <= idx; i++) {
-            if ((n & (1 << i)) == 0) {
-                sum += 1 << i;
-            }
-        }
-        
-        return n + sum;
+        int noOfBits = log2(n) + 1;
+        return (1<<noOfBits)-1;
     }
 };

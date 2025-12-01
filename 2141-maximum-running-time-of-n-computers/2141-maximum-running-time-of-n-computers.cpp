5ad95df8f1607d2928203460280eada84914a5f4
Rana Dutta <duttarana189@gmail.com>
2025-12-01 08:46:46 +0530

    Time: 27 ms (44.27%), Space: 59.7 MB (88.85%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPossible(const vector<int>& nums, long long hours, int n){
+        long long tot = 1LL * n * hours; 
+        for(int num: nums){
+            tot -= min((long long)num, hours);
+            if(tot <= 0) return true; 
+        }
+        return tot <= 0;
+    }
+
+    long long maxRunTime(int n, vector<int>& nums) {
+        long long low = 0;
+        long long high = accumulate(nums.begin(), nums.end(), 0LL);
+        long long ans = 0;
+        while(low <= high){
+            long long mid = low + (high - low) / 2;
+            if(isPossible(nums, mid, n)){
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};

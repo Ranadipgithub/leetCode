e83370ba7a625636e5f066169c1c5f36f883461b
Rana Dutta <duttarana189@gmail.com>
2025-12-29 12:38:07 +0530

    Time: 0 ms (100%), Space: 18.7 MB (8.94%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &image, int i, int j, int color, int initial){
-        int n = image.size();
-        int m = image[0].size();
-        if(i<0 || i>=n || j<0 || j>=m || image[i][j] != initial){
-            return;
-        }
-        image[i][j] = color;
-        dfs(image, i+1, j, color, initial);
-        dfs(image, i-1, j, color, initial);
-        dfs(image, i, j+1, color, initial);
-        dfs(image, i, j-1, color, initial);
-    }
+    int n, m;
+    vector<vector<int>> directions{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        int initial = image[sr][sc];
-        if(initial == color) return image;
-        dfs(image, sr, sc, color, initial);
+        n = image.size();
+        m = image[0].size();
+
+        int orig = image[sr][sc];
+
+        if (orig == color) return image;
+
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        image[sr][sc] = color;
+        while(!q.empty()){
+            auto [x, y] = q.front();
+            q.pop();
+
+            for(auto &dir: directions){
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m || image[nx][ny] != orig) continue;
+                if(image[nx][ny] == orig){
+                    image[nx][ny] = color;
+                    q.push({nx, ny});
+                }
+            }
+        }
         return image;
     }
 };

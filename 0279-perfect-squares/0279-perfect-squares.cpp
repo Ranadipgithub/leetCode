e1e70b7a0338e2836e57df187de1cadd4dec9b45
Rana Dutta <duttarana189@gmail.com>
2026-02-07 10:43:57 +0530

    Time: 72 ms (43.77%), Space: 8.4 MB (94.89%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(int n, int m){
-        if (n == 0) return 0;           
-        if (n < 0 || m == 0) return 1e9;
-
-        if(dp[n][m] != -1) return dp[n][m];
-
-        int take = 1e9;
-        if (m * m <= n)  
-            take = 1 + solve(n - m * m, m);
-
-        int not_take = solve(n, m - 1);
-
-        return dp[n][m] = min(take, not_take);
+    int dp[10001];
+    int solve(int n){
+        if(n == 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        int mini = INT_MAX;
+        for(int i = sqrt(n);i>=1;i--){
+            mini = min(mini, 1 + solve(n - i*i));
+        }
+        return dp[n] = mini;
     }
-
     int numSquares(int n) {
-        int m = sqrt(n);
-        dp.resize(n+1, vector<int>(m+1, -1));
-        return solve(n, m);
+        memset(dp, -1, sizeof(dp));
+        return solve(n);
     }
 };

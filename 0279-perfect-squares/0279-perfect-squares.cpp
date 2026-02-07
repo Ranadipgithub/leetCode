c88d4b42d42013d779415a1d05c2f4e5507c3b31
Rana Dutta <duttarana189@gmail.com>
2026-02-07 10:59:22 +0530

    Time: 48 ms (57.84%), Space: 13 MB (81.56%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -11,7 +11,20 @@ public:
         return dp[n] = mini;
     }
     int numSquares(int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(n);
+        // memset(dp, -1, sizeof(dp));
+        // return solve(n);
+
+        if(n <= 3) return n;
+
+        vector<int> t(n+1);
+        t[1] = 1, t[2] = 2, t[3] = 3;
+        for(int i = 4;i<=n;i++){
+            int mini = INT_MAX;
+            for(int j = sqrt(i);j>=1;j--){
+                mini = min(mini, 1 + t[i-j*j]);
+            }
+            t[i] = mini;
+        }
+        return t[n];
     }
 };

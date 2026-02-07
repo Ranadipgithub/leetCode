3ebc1a23b6ecc800e8021a11d0fae496104d0235
Rana Dutta <duttarana189@gmail.com>
2026-02-07 12:01:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (88.74%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
     int dp[101];
-
-    int solve(string s, int n, int idx){
-        if(idx == n) return 1;
+    int solve(string &s, int idx){
+        int n = s.size();
         if(dp[idx] != -1) return dp[idx];
-
-        int cnt = 0;
-        if(s[idx] != '0') {
-            cnt += solve(s, n, idx+1);
-            if(idx + 1 < n){
-                string news = s.substr(idx, 2);
-                if(stoi(news) <= 26){
-                    cnt += solve(s, n, idx+2);
-                }
+        if(idx == n) return 1;
+        if(s[idx] == '0') return 0;
+        int ways = 0;
+        if(idx + 1 < n){
+            int num = (s[idx] - '0') * 10 + (s[idx+1]-'0');
+            if(num >= 10 && num <= 26){
+                ways += solve(s, idx+2);
             }
         }
-
-        return dp[idx] = cnt;
+        ways += solve(s, idx+1);
+        return dp[idx] = ways;
     }
-
     int numDecodings(string s) {
         int n = s.size();
         memset(dp, -1, sizeof(dp));
-        if(s[0] == '0') return 0;
-
-        for(int i = 1; i < n; i++){
-            if(s[i] == '0'){
-                if(s[i-1] != '1' && s[i-1] != '2') return 0;
-            }
-        }
-
-        return solve(s, n, 0);
+        return solve(s, 0);
     }
 };

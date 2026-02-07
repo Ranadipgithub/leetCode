4ea2bf4d792915a8a0a111cd34c1bae2a59d345a
Rana Dutta <duttarana189@gmail.com>
2026-02-07 15:30:02 +0530

    Time: 3 ms (6.19%), Space: 10.2 MB (78.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,39 +1,19 @@
 class Solution {
 public:
-    int dp[101];
+    int dp[105];
     int solve(vector<int>&nums, int idx){
         int n = nums.size();
         if(idx >= n) return 0;
         if(dp[idx] != -1) return dp[idx];
-        int steal = nums[idx] + solve(nums, idx+2);
+        int take = 0;
+        if(idx < n)
+            take += nums[idx] + solve(nums, idx+2);
         int skip = solve(nums, idx+1);
-        return dp[idx] = max(steal, skip);
+        return dp[idx] = max(take, skip);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        // memset(dp, -1, sizeof(dp));
-        // return solve(nums, 0);
-        // bottom up
-        // vector<int>t(n+1, 0);
-        // t[0] = 0;
-        // t[1] = nums[0];
-        // for(int i = 2;i<=n;i++){
-        //     int steal = nums[i-1] + t[i-2];
-        //     int skip = t[i-1];
-        //     t[i] = max(steal, skip);
-        // }
-        // return t[n];
-
-        // const space
-        int prevprev = 0;
-        int prev = nums[0];
-        for(int i = 2;i<=n;i++){
-            int steal = nums[i-1] + prevprev;
-            int skip = prev;
-            int temp = max(steal, skip);
-            prevprev = prev;
-            prev = temp;
-        }
-        return prev;
+        memset(dp, -1, sizeof(dp));
+        return solve(nums, 0);
     }
 };

dc9e3373fe73988a57f2301e73d1c8f04e1a2278
Rana Dutta <duttarana189@gmail.com>
2026-02-08 14:26:11 +0530

    Time: 0 ms (100%), Space: 16.2 MB (68.42%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -3,8 +3,8 @@ public:
     int dp[20001];
     int solve(vector<int>&temp, int idx){
         int n = temp.size();
-        if(dp[idx] != -1) return dp[idx];
         if(idx >= n) return 0;
+        if(dp[idx] != -1) return dp[idx];
         int take = 0;
         if(idx < n) take = temp[idx] + solve(temp, idx+2);
         int skip = solve(temp, idx+1);
@@ -13,14 +13,9 @@ public:
     int deleteAndEarn(vector<int>& nums) {
         int n = nums.size();
         memset(dp, -1, sizeof(dp));
-        unordered_map<int, int> mpp;
-        for(auto it: nums) mpp[it] += it;
         int maxi = *max_element(nums.begin(), nums.end());
-        vector<int> temp(maxi+1);
-        for(int i = 0;i<temp.size();i++){
-            temp[i] = mpp[i];
-            cout << temp[i] << " ";
-        }
+        vector<int> temp(maxi + 1, 0);
+        for(int x : nums) temp[x] += x;
         return solve(temp, 0);
     }
 };

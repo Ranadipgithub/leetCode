c614098158a8b04d34865e938e3bc0828eef422b
Rana Dutta <duttarana189@gmail.com>
2026-02-08 10:27:40 +0530

    Time: 27 ms (63.84%), Space: 16.4 MB (68.05%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(vector<int>&coins, int amt, int n){
-        if(n == 0){
-            if(amt == 0) return 1;
-            return 0;
+    int dp[5005][305];
+    int solve(int amount, int idx, vector<int>&coins){
+        if(amount == 0) return 1;
+        else if(idx >= coins.size()) return 0;
+        if(dp[amount][idx] != -1) return dp[amount][idx];
+        int take = 0;
+        if(amount >= coins[idx]){
+            take += solve(amount-coins[idx], idx, coins);
         }
-        if(dp[n][amt] != -1) return dp[n][amt];
-        if(coins[n-1] <= amt){
-            return dp[n][amt] = solve(coins, amt-coins[n-1], n) + solve(coins, amt, n-1);
-        } else{
-            return dp[n][amt] = solve(coins, amt, n-1);
-        }
+        int skip = solve(amount, idx+1, coins);
+        return dp[amount][idx] = take + skip;
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        dp.assign(n+1, vector<int>(amount+1, -1));
-        return solve(coins, amount, n);
+        memset(dp, -1, sizeof(dp));
+        return solve(amount, 0, coins);
     }
 };

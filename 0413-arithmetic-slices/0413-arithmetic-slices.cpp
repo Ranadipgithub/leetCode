40bcc8c3e8f16526317e7f2f9757896129563df9
Rana Dutta <duttarana189@gmail.com>
2026-02-08 15:36:02 +0530

    Time: 11 ms (3.96%), Space: 10.9 MB (21.68%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        // 3: n-3+1
+        // 4: n-4+1
+        // 5: n-5+1
+        // (n-2) + (n-3) + (n-4) + ... + 1 = (n-2)(n-1)/2
+
+        int n = nums.size();
+        vector<int> diff;
+        for(int i = 1;i<n;i++){
+            diff.push_back(nums[i]-nums[i-1]);
+        }
+        // 1 1 1 2 3 4 4 5 5 
+        int ans = 0;
+        for(int i = 0;i<diff.size();i++){
+            int cnt = 1, j = i+1;
+            for(;j<diff.size();j++){
+                if(diff[i] == diff[j]) cnt++;
+                else break;
+            }
+            i = j-1;
+            cout << cnt << " ";
+            ans += cnt * (cnt-1)/2;
+        }
+        return ans;
+    }
+};

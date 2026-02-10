fa61d024b0cb1e54569596063f3b608f0c39ea32
Rana Dutta <duttarana189@gmail.com>
2026-02-10 14:49:26 +0530

    Time: 3 ms (38.37%), Space: 9 MB (90.53%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[1001];
+    int solve(vector<int>&nums, int target){
+        if(target < 0) return 0;
+        if(target == 0) return 1;
+        if(dp[target] != -1) return dp[target];
+        int ways = 0;
+        for(int i = 0;i<nums.size();i++){
+            ways += solve(nums, target-nums[i]);
+        }
+        return dp[target] = ways;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        int n =  nums.size();
+        memset(dp, -1, sizeof(dp));
+        return solve(nums, target);
+    }
+};

c57f9f9073757720857be27688fda4685e1ebd60
Rana Dutta <duttarana189@gmail.com>
2026-02-11 14:58:57 +0530

    Time: 433 ms (32.12%), Space: 213.8 MB (32.89%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -13,7 +13,26 @@ public:
         return dp[idx][canBuy][cnt] = profit;
     }
     int maxProfit(vector<int>& prices) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 1, 0, prices);
+        // memset(dp, -1, sizeof(dp));
+
+        // return solve(0, 1, 0, prices);
+
+        int n = prices.size();
+        vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for(int idx = n-1;idx>=0;idx--){
+            for(int canBuy = 0;canBuy <= 1;canBuy++){
+                for(int cnt = 0;cnt < 2;cnt++){
+                    int profit = 0;
+                    if(canBuy){
+                        profit += max((-prices[idx] + dp[idx+1][0][cnt]), dp[idx+1][1][cnt]);
+                    } else {
+                        profit += max((prices[idx] + dp[idx+1][1][cnt+1]), dp[idx+1][0][cnt]);
+                    }
+                    dp[idx][canBuy][cnt] = profit;
+                }
+            }
+        }
+        return dp[0][1][0];
     }
 };

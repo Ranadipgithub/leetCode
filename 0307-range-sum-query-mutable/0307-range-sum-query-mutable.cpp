8ee7596f4fbe4fd3ba10f1c22c0118011c05e7f7
Rana Dutta <duttarana189@gmail.com>
2026-02-11 21:50:42 +0530

    Time: 62 ms (52.84%), Space: 182.3 MB (35.99%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -2,53 +2,55 @@ class NumArray {
 public:
     vector<int> segTree;
     int n;
-
-    void buildSegTree(int i, int l, int r, vector<int>& nums) {
-        if (l == r) {
+    void buildSegTree(vector<int>&nums, int i, int l, int r){
+        if(l == r){
             segTree[i] = nums[l];
             return;
         }
-        int mid = l + (r - l) / 2;
-        buildSegTree(2*i + 1, l, mid, nums);
-        buildSegTree(2*i + 2, mid + 1, r, nums);
-        segTree[i] = segTree[2*i + 1] + segTree[2*i + 2];
+        int mid = (l+r)/2;
+        buildSegTree(nums,2*i+1, l, mid);
+        buildSegTree(nums, 2*i+2, mid+1, r);
+        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
     }
-
-    void updateSegTree(int idx, int val, int i, int l, int r) {
-        if (l == r) {
+    void updateSegTree(int idx, int val, int i, int l, int r){
+        if(l == r){
             segTree[i] = val;
             return;
         }
-        int mid = l + (r - l) / 2;
-        if (idx <= mid)
-            updateSegTree(idx, val, 2*i + 1, l, mid);
-        else
-            updateSegTree(idx, val, 2*i + 2, mid + 1, r);
-        segTree[i] = segTree[2*i + 1] + segTree[2*i + 2];
+        int mid = (l+r)/2;
+        if(idx <= mid){
+            updateSegTree(idx, val, 2*i+1, l, mid);
+        } else {
+            updateSegTree(idx, val, 2*i+2, mid+1, r);
+        }
+        segTree[i] = segTree[2*i+1] + segTree[2*i+2];
     }
-
-    int findRangeSum(int left, int right, int i, int l, int r) {
-        if (right < l || left > r)
+    int rangeQuery(int left, int right, int i, int l, int r){
+        if(r < left || l > right){
             return 0;
-        if (l >= left && r <= right)
+        } else if(l >= left && r <= right){
             return segTree[i];
-        int mid = l + (r - l) / 2;
-        return findRangeSum(left, right, 2*i + 1, l, mid)
-             + findRangeSum(left, right, 2*i + 2, mid + 1, r);
-    }
+        } else {
+            int mid = (l+r)/2;
+            int res1 = rangeQuery(left, right, 2*i+1, l, mid);
+            int res2 = rangeQuery(left, right, 2*i+2, mid+1, r);
 
+            return res1+res2;
+        }
+    }
     NumArray(vector<int>& nums) {
         n = nums.size();
-        segTree.assign(4 * n, 0);
-        buildSegTree(0, 0, n - 1, nums);
-    }
+        segTree.resize(4*n, 0);
 
+        buildSegTree(nums, 0, 0, n-1);
+    }
+    
     void update(int index, int val) {
-        updateSegTree(index, val, 0, 0, n - 1);
+        updateSegTree(index, val, 0, 0, n-1);
     }
-
+    
     int sumRange(int left, int right) {
-        return findRangeSum(left, right, 0, 0, n - 1);
+        return rangeQuery(left, right, 0, 0, n-1);
     }
 };
 

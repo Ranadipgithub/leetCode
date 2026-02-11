cba693ca395748717ab665e1ccc5162c46ded620
Rana Dutta <duttarana189@gmail.com>
2026-02-11 14:59:17 +0530

    Time: 399 ms (44.23%), Space: 211.5 MB (43.9%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int dp[100001][2][3];
-    int solve(int idx, int canBuy, int cnt, vector<int>&prices){
-        if(cnt >= 2 || idx >= prices.size()) return 0;
-        if(dp[idx][canBuy][cnt] != -1) return dp[idx][canBuy][cnt];
-        int profit = 0;
-        if(canBuy){
-            profit += max((-prices[idx] + solve(idx+1, 0, cnt, prices)), solve(idx+1, 1, cnt, prices));
-        } else {
-            profit += max((prices[idx] + solve(idx+1, 1, cnt+1, prices)), solve(idx+1, 0, cnt, prices));
-        }
-        return dp[idx][canBuy][cnt] = profit;
-    }
+    // int dp[100001][2][3];
+    // int solve(int idx, int canBuy, int cnt, vector<int>&prices){
+    //     if(cnt >= 2 || idx >= prices.size()) return 0;
+    //     if(dp[idx][canBuy][cnt] != -1) return dp[idx][canBuy][cnt];
+    //     int profit = 0;
+    //     if(canBuy){
+    //         profit += max((-prices[idx] + solve(idx+1, 0, cnt, prices)), solve(idx+1, 1, cnt, prices));
+    //     } else {
+    //         profit += max((prices[idx] + solve(idx+1, 1, cnt+1, prices)), solve(idx+1, 0, cnt, prices));
+    //     }
+    //     return dp[idx][canBuy][cnt] = profit;
+    // }
     int maxProfit(vector<int>& prices) {
         // memset(dp, -1, sizeof(dp));
 

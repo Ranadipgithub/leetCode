7852518f66eb8aa64d74b3fb069a038d6d3cef56
Rana Dutta <duttarana189@gmail.com>
2026-02-11 14:39:58 +0530

    Time: 7 ms (21.91%), Space: 24.6 MB (7.76%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int solve(vector<int>&prices, int index, int canBuy){
-        if(index == prices.size()){
-            return 0;
-        }
-        if(dp[index][canBuy] != -1) return dp[index][canBuy];
-        int profit = 0;
-        if(canBuy == 1){
-            profit = max((-prices[index] + solve(prices, index+1, 0)), 0 + solve(prices, index+1, 1));
-        } else{
-            profit = max((prices[index] + solve(prices, index+1, 1)), 0 + solve(prices, index+1, 0));
-        }
-        return dp[index][canBuy] = profit;
-    }
+    // vector<vector<int>> dp;
+    // int solve(vector<int>&prices, int idx, int canBuy){
+    //     if(idx == prices.size()){
+    //         return 0;
+    //     }
+    //     if(dp[idx][canBuy] != -1) return dp[idx][canBuy];
+    //     int profit = 0;
+    //     if(canBuy == 1){
+    //         profit = max((-prices[idx] + solve(prices, idx+1, 0)), 0 + solve(prices, idx+1, 1));
+    //     } else{
+    //         profit = max((prices[idx] + solve(prices, idx+1, 1)), 0 + solve(prices, idx+1, 0));
+    //     }
+    //     return dp[idx][canBuy] = profit;
+    // }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        dp.resize(n, vector<int>(2, -1));
-        return solve(prices, 0, 1);
+        // dp.resize(n, vector<int>(2, -1));
+
+        vector<vector<int>> dp(n+1, vector<int> (2, 0));
+        dp[n][1] = dp[n][0] = 0;
+        for(int idx = n-1;idx>=0;idx--){
+            for(int canBuy = 0;canBuy<=1;canBuy++){
+                int profit = 0;
+                if(canBuy == 1){
+                    profit = max((-prices[idx] + dp[idx+1][0]), 0 + dp[idx+1][1]);
+                } else{
+                    profit = max((prices[idx] + dp[idx+1][1]), 0 + dp[idx+1][0]);
+                }
+                dp[idx][canBuy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };

b0e5d6f479eaab3af956d18b03cdb6a6b16b943e
Rana Dutta <duttarana189@gmail.com>
2026-02-17 23:55:19 +0530

    Time: 0 ms (100%), Space: 9.7 MB (41.32%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> result;
+
+        for (int i = 0; i <= 11; i++) {
+            for (int j = 0; j <= 59; j++) {
+                if (__builtin_popcount(i) + __builtin_popcount(j) == turnedOn) {
+                    string hr = to_string(i);
+                    string minute = (j < 10 ? "0" : "") + to_string(j);
+                    
+                    result.push_back(hr + ":" + minute);
+                }
+            }
+        }
+        return result;
+    }
+};

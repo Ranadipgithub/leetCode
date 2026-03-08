77c0eb07ed5cf5060e6a13ea847976e80178e97a
Rana Dutta <duttarana189@gmail.com>
2026-03-08 19:46:37 +0530

    Time: 8 ms (25.18%), Space: 26.6 MB (30.33%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int solve(int i, int j, string &s, int cnt){
-        if(cnt > 1) return 2;
-        if(i >= j) return cnt;
-
-        if(s[i] == s[j]){
-            return solve(i+1, j-1, s, cnt);
-        } 
-        else {
-            int cnt1 = solve(i+1, j, s, cnt+1);
-            int cnt2 = solve(i, j-1, s, cnt+1);
-            return min(cnt1, cnt2);
+    bool check(string s, int idx){
+        int i = 0, j = s.size()-1;
+        while(i < j){
+            if(i == idx){
+                i++;
+                continue;
+            }
+            if(j == idx){
+                j--;
+                continue;
+            }
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
         }
+        return true;
     }
 
     bool validPalindrome(string s) {
-        int cnt = solve(0, s.size()-1, s, 0);
-        return cnt <= 1;
+        int i = 0, j = s.size()-1;
+
+        while(i < j){
+            if(s[i] != s[j]){
+                return check(s, i) || check(s, j);
+            }
+            i++;
+            j--;
+        }
+
+        return true;
     }
 };

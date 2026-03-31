5c2af130b7b48145d9c21d5944a6a3b043796b60
Rana Dutta <duttarana189@gmail.com>
2026-03-31 12:48:11 +0530

    Time: 279 ms (37.28%), Space: 107.6 MB (87.85%) - LeetHub

--- 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.cpp
+++ 2608-shortest-cycle-in-a-graph/2608-shortest-cycle-in-a-graph.cpp
@@ -2,15 +2,40 @@ class Solution {
 public:
     int findShortestCycle(int n, vector<vector<int>>& edges) {
         unordered_map<int, vector<int>> adj;
+        vector<int> degree(n, 0);
         for(auto &edge: edges){
             int u = edge[0], v = edge[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
+            degree[u]++;
+            degree[v]++;
+        }
+
+        vector<bool> pruned(n, false);
+        queue<int> leaves;
+        for(int i = 0;i<n;i++){
+            if(degree[i] <= 1){
+                leaves.push(i);
+            }
+        }
+
+        while(!leaves.empty()){
+            int u = leaves.front();
+            leaves.pop();
+            pruned[u] = true;
+
+            for(int &v: adj[u]){
+                if(!pruned[v]){
+                    degree[v]--;
+                    if(degree[v] == 1) leaves.push(v);
+                }
+            }
         }
 
         int minLen = INT_MAX;
         
         for(int i = 0; i < n; i++){ 
+            if(pruned[i]) continue;
             vector<int> dist(n, -1);
             dist[i] = 0;
             queue<pair<int, int>> q;

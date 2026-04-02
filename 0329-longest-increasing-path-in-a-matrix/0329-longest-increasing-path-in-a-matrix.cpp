37f1896884450fb3665b2b1c3f71e42bbda5fe0e
Rana Dutta <duttarana189@gmail.com>
2026-04-02 23:08:22 +0530

    Time: 64 ms (14.94%), Space: 37 MB (11.5%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>> directions{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
-    int dfs(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& dp) {
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int maxLen = 1;
-
-        for (auto& dir : directions) {
-            int newi = i + dir[0];
-            int newj = j + dir[1];
-
-            if (newi >= 0 && newi < matrix.size() && newj >= 0 && newj < matrix[0].size()
-                && matrix[newi][newj] > matrix[i][j]) {
-                maxLen = max(maxLen, 1 + dfs(matrix, newi, newj, dp));
-            }
-        }
-
-        dp[i][j] = maxLen;
-        return maxLen;
-    }
-
+    vector<vector<int>> directions{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        int maxCnt = 0;
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                maxCnt = max(maxCnt, dfs(matrix, i, j, dp));
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> adj(n * m);
+        vector<int> indegree(n * m, 0);
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                for(auto &dir: directions){
+                    int newi = i + dir[0];
+                    int newj = j + dir[1];
+                    if(newi < 0 || newi >= n || newj < 0 || newj >= m) continue;
+                    if(matrix[newi][newj] <= matrix[i][j]) continue;
+                    int u = newi * m + newj;
+                    int v = i * m + j;
+                    adj[u].push_back(v);
+                    indegree[v]++;
+                }
             }
         }
 
-        return maxCnt;
+        queue<int> q;
+        for(int i = 0; i < n * m; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        int len = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            len++;
+            while(sz--){
+                int u = q.front();
+                q.pop();
+
+                for(int &v: adj[u]){
+                    indegree[v]--;
+                    if(indegree[v] == 0) q.push(v);
+                }
+            }
+        }
+        return len;
     }
 };

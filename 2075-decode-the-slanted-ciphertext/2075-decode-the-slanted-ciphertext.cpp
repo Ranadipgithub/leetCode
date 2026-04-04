4f962ce980dee62f252b2ab99e09507c32e1a6b8
Rana Dutta <duttarana189@gmail.com>
2026-04-04 10:13:11 +0530

    Time: 28 ms (66.95%), Space: 41.7 MB (63.56%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -3,19 +3,16 @@ public:
     string decodeCiphertext(string s, int rows) {
         int n = s.size();
         int cols = n/rows;
-        unordered_map<int, string> mpp;
-        for(int i = 0;i<rows;i++){
-            for(int j = 0;j<cols;j++){
-                if(i-j > 0) continue;
-                int idx = (i*cols)+j;
-                mpp[j-i] += s[idx];
+        string res = "";
+        for(int j = 0;j<cols;j++){
+            int row = 0, col = j;
+            while(row < rows && col < cols){
+                int idx = (row*cols) + col;
+                res += s[idx];
+                row++;
+                col++;
             }
         }
-        string res = "";
-        for (int i = 0; i < cols; i++) {
-            res += mpp[i];
-        }
-        
         while (!res.empty() && res.back() == ' ') {
             res.pop_back();
         }

e87e1f30520162e8916c4e18468345dde779d321
Rana Dutta <duttarana189@gmail.com>
2026-04-04 10:36:05 +0530

    Time: 87 ms (34.85%), Space: 148.8 MB (29.15%) - LeetHub

---
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        unordered_map<int, vector<int>> adj;
+        vector<int> indegree(n, 0);
+        for(auto &edge: relations){
+            int u = edge[0]-1, v = edge[1]-1;
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue<int> q;
+        for(int i = 0;i<n;i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int> maxiTime = time;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+
+            for(int &v: adj[u]){
+                maxiTime[v] = max(maxiTime[v], maxiTime[u]+time[v]);
+                indegree[v]--;
+                if(indegree[v] == 0) q.push(v);
+            }
+        }
+        return *max_element(maxiTime.begin(), maxiTime.end());
+    }
+};

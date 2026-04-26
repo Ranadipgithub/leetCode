88ca4f3bf1e55d5a76616aeddb840919c4c1cb4b
Rana Dutta <duttarana189@gmail.com>
2026-04-26 10:31:47 +0530

    Time: 161 ms (32.38%), Space: 27.9 MB (34.49%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -4,18 +4,22 @@ public:
     int solve(int curr, int last, vector<pair<int, int>> &players){
         if(curr >= players.size()) return 0;
         if(dp[curr+1][last+1] != -1) return dp[curr+1][last+1];
-        int take = 0, skip = 0;
-        if(last == -1) {
+        int take = 0;
+        if(last == -1 || players[curr].second >= players[last].second){
             take = players[curr].second + solve(curr+1, curr, players);
-            skip = solve(curr+1, last, players);
-        } else {
-            if(players[curr].first > players[last].first && players[curr].second < players[last].second){
-                skip = solve(curr+1, last, players);
-            } else {
-                take = players[curr].second + solve(curr+1, curr, players);
-                skip = solve(curr+1, last, players);
-            }
         }
+        int skip = solve(curr+1, last, players);
+        // if(last == -1) {
+        //     take = players[curr].second + solve(curr+1, curr, players);
+        //     skip = solve(curr+1, last, players);
+        // } else {
+        //     if(players[curr].first > players[last].first && players[curr].second < players[last].second){
+        //         skip = solve(curr+1, last, players);
+        //     } else {
+        //         take = players[curr].second + solve(curr+1, curr, players);
+        //         skip = solve(curr+1, last, players);
+        //     }
+        // }
         return dp[curr+1][last+1] = max(take, skip);
     }
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {

2205a57192a89bb6c1a7f2ecec8119cb8161d0b8
Rana Dutta <duttarana189@gmail.com>
2026-05-05 21:29:29 +0530

    Time: 39 ms (70.6%), Space: 73 MB (36.09%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        for(auto &edge: prerequisites){
+            int u = edge[0], v = edge[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        queue<int> q;
+        for(int i = 0;i<numCourses;i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        unordered_map<int, unordered_set<int>> mpp;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(int &ngbr: adj[node]){
+                mpp[ngbr].insert(node);
+                for(auto &p: mpp[node]){
+                    mpp[ngbr].insert(p);
+                }
+                indegree[ngbr]--;
+                if(indegree[ngbr] == 0) q.push(ngbr);
+            }
+        }
+        vector<bool> res;
+        for(auto &q: queries){
+            int u = q[0], v = q[1];
+            if(mpp[v].count(u)) res.push_back(true);
+            else res.push_back(false);
+        }
+        return res;
+    }
+};

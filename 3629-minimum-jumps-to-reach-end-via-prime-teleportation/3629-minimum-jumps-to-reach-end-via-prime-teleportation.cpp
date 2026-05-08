0eb2c5ffcd79c9632172ff0b3ac64f766e9eeb02
Rana Dutta <duttarana189@gmail.com>
2026-05-08 11:14:52 +0530

    Time: 538 ms (78.97%), Space: 274.4 MB (82.66%) - LeetHub

--- 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
+++ 3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation.cpp
@@ -1,74 +1,65 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> mpp;
-    unordered_map<int, vector<int>> adj;
-    
-    void sieve(int n) {
-        vector<bool> isPrime(n + 1, true);
+    void sieve(int n, vector<bool>&isPrime){
+        isPrime.assign(n+1, true);
         isPrime[0] = isPrime[1] = false;
-
-        for (int i = 2; i <= n; i++) {
-            if (isPrime[i]) {
-                for (long long j = 1LL * i * i; j <= n; j += i) {
-                    isPrime[j] = false;
-                }
-                
-                if (mpp.count(i)) {
-                    for (int j = i; j <= n; j += i) {
-                        if (mpp.count(j)) {
-                            for (int idx : mpp[j]) {
-                                adj[i].push_back(idx); 
-                            }
-                        }
-                    }
+        for(int p = 2;p*p<=n;p++){
+            if(isPrime[p]){
+                for(int i = p*p;i<=n;i+=p){
+                    isPrime[i] = false;
                 }
             }
         }
     }
-
     int minJumps(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0; i < n; i++){
+        int maxi = *max_element(nums.begin(), nums.end());
+        static vector<bool> isPrime;
+        static bool built = false;
+        if(!built){
+            built = true;
+            sieve(1e6, isPrime);
+        }
+        unordered_map<int, vector<int>> mpp;
+        for(int i = 0;i<n;i++){
             mpp[nums[i]].push_back(i);
         }
-        int maxi = *max_element(nums.begin(), nums.end());
-        sieve(maxi);
-
         queue<int> q;
-        vector<int> visited(n, 0);
         q.push(0);
+        vector<int> visited(n, 0);
         visited[0] = 1;
-        
-        int cnt = 0; 
-        
+        int steps = 0;
         while(!q.empty()){
             int size = q.size();
             while(size--){
                 int u = q.front();
                 q.pop();
 
-                if(u == n-1) return cnt;
-
-                if(u+1 < n && !visited[u+1]) {
+                if(u == n-1) return steps;
+                if(u+1 < n && !visited[u+1]){
                     visited[u+1] = 1;
                     q.push(u+1);
                 }
-
-                if(u-1 >= 0 && !visited[u-1]){
+                if(u-1>=0 && !visited[u-1]){
                     visited[u-1] = 1;
                     q.push(u-1);
                 }
-
-                for(int v: adj[nums[u]]){
-                    if(!visited[v]){
-                        visited[v] = 1;
-                        q.push(v);
+                if(isPrime[nums[u]]){
+                    for(int i = nums[u];i<=maxi;i+= nums[u]){
+                        if(mpp.count(i)){
+                            for(auto &idx: mpp[i]){
+                                if(!visited[idx]){
+                                    visited[idx] = 1;
+                                    q.push(idx);
+                                }
+                            }
+                            mpp[i].clear();
+                        }
                     }
                 }
-                adj[nums[u]].clear(); 
             }
-            cnt++;
+            steps++;
         }
-        return cnt;
+        return -1;
     }
 };

cabac8e113f659ede260e90337e20da85a6c4b2f
Rana Dutta <duttarana189@gmail.com>
2026-05-14 08:49:25 +0530

    Time: 0 ms (100%), Space: 27.6 MB (11.11%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isGood(vector<int>& nums) {
         int maxi = *max_element(nums.begin(), nums.end());
-        unordered_map<int, int> fq;
+        vector<int> fq(maxi+1, 0);
         for(int num: nums) fq[num]++;
         for(int i = 1;i<maxi;i++){
             if(fq[i] != 1) return false;

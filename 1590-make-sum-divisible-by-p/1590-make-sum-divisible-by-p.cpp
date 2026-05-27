c51f1edf2dccfceb58916c4d8de351492b6687a6
Rana Dutta <duttarana189@gmail.com>
2026-05-27 12:05:40 +0530

    Time: 72 ms (75.49%), Space: 87.8 MB (65.92%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
+        // let's say we want to remove the subarray from i+1...j
+        // then the sum of rem should be divisible by p
+        // (pre[i] + pre[n-1] - pre[j])%p = 0;
+        // pre[i]%p = (pre[j]%p - total%p + p)%p
         int n = nums.size();
-        long long sum = accumulate(nums.begin(), nums.end(), 0LL);
-        int mod = sum%p;
-        if(mod == 0) return 0;
         unordered_map<int, int> mpp;
+        long long sum = 0;
+        int mini = n;
         mpp[0] = -1;
-        long long pre = 0;
-        int ans = INT_MAX;
+        long long total = accumulate(nums.begin(), nums.end(), 0LL);
+        if(total % p == 0) return 0;
         for(int i = 0;i<n;i++){
-            pre = (pre + (long long)nums[i])%p;
-            int need = (pre - mod+p)%p;
-            if(mpp.count(need)){
-                ans = min(ans, i-mpp[need]);
+            sum += nums[i];
+            int curr = (sum % p + p) % p;
+            int rem = (sum%p - total%p + p) % p;
+            if(mpp.count(rem)){
+                mini = min(mini, i - mpp[rem]);
             }
-            mpp[pre] = i;
+            mpp[curr] = i;
         }
-        return (ans == INT_MAX || ans == n)? -1: ans;
+        return mini == nums.size()? -1: mini;
     }
 };

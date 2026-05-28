e8432124461724e9d3f7be41460094ef99f83663
Rana Dutta <duttarana189@gmail.com>
2026-05-28 13:24:11 +0530

    Time: 92 ms (24.27%), Space: 148.1 MB (12.14%) - LeetHub

--- 3153-sum-of-digit-differences-of-all-pairs/3153-sum-of-digit-differences-of-all-pairs.cpp
+++ 3153-sum-of-digit-differences-of-all-pairs/3153-sum-of-digit-differences-of-all-pairs.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     long long sumDigitDifferences(vector<int>& nums) {
         int n = nums.size();
-        string str = to_string(nums[0]);
-        int size = str.size();
+        vector<string> snums(n);
+        for(int i = 0;i<n;i++){
+            snums[i] = to_string(nums[i]);
+        }
+        int size = snums[0].size();
         long long total = 0;
         for(int i = 0;i<size;i++){
             vector<int> mpp(10, 0);
             long long cnt = 0;
             for(int j = 0;j<n;j++){
-                string str = to_string(nums[j]);
-                char ch = str[i];
+                char ch = snums[j][i];
                 cnt += j - mpp[ch-'0'];
                 mpp[ch-'0']++;
             }

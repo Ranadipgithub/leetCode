25b6202bc48dbd922fc1e6cb407d455ffae6668b
Rana Dutta <duttarana189@gmail.com>
2026-06-07 13:52:18 +0530

    Time: 10 ms (85.8%), Space: 30.2 MB (72.04%) - LeetHub

--- 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.cpp
+++ 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    void solve(string curr, vector<string> &res, int n, char prev, int k, int idx){
+    void solve(string &curr, vector<string> &res, int n, char prev, int k, int idx){
         if(n <= 0){
-            cout << curr << endl;
             res.push_back(curr);
             return;
         }
-        solve(curr+'0', res, n-1, '0', k, idx+1);
+        curr.push_back('0');
+        solve(curr, res, n-1, '0', k, idx+1);
+        curr.pop_back();
         if(prev != '1' && k >= idx){
-            solve(curr+'1', res, n-1, '1', k-idx, idx+1);
+            curr.push_back('1');
+            solve(curr, res, n-1, '1', k-idx, idx+1);
+            curr.pop_back();
         }
     }
     vector<string> generateValidStrings(int n, int k) {
         vector<string> res;
-        solve("", res, n, '0', k, 0);
+        string temp = "";
+        solve(temp, res, n, '0', k, 0);
         return res;
     }
 };

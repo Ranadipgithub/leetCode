290086a39277789d2c3fc58c9882219cec5da1ab
Rana Dutta <duttarana189@gmail.com>
2026-06-09 10:15:16 +0530

    Time: 4 ms (33.07%), Space: 103.8 MB (3.11%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -3,7 +3,6 @@ public:
     long long maxTotalValue(vector<int>& nums, int k) {
         int maxi = *max_element(nums.begin(), nums.end());
         int mini = *min_element(nums.begin(), nums.end());
-        long long res =  (long long)k*(maxi-mini)*1LL;
-        return res;
+        return 1LL*k*(maxi-mini);
     }
 };

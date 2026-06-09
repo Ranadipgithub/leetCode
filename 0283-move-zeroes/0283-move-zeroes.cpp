10716f91c24917b227a74c393de7000d5b639f66
Rana Dutta <duttarana189@gmail.com>
2026-06-09 10:52:50 +0530

    Time: 0 ms (100%), Space: 23.9 MB (54.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,11 +3,13 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         int i = 0;
-        for(int j = 0;j<n;j++){
+        for(;i<n;i++){
+            if(nums[i] == 0) break;
+        }
+        for(int j = i+1;j<n;j++){
             if(nums[j] != 0){
-                nums[i++] = nums[j];
+                swap(nums[i++], nums[j]);
             }
         }
-        for(int k = i;k<n;k++) nums[k] = 0;
     }
 };

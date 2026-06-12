bb25fa15b8b63ce05cff7d177322e407e67d6543
Rana Dutta <duttarana189@gmail.com>
2026-06-12 19:12:34 +0530

    Time: 95 ms (30.23%), Space: 165 MB (27.67%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size(), m = nums2.size();
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        int i = 0, j = 0;
-        vector<vector<int>> res;
         pq.push({nums1[0]+nums2[0], {0, 0}});
-        set<pair<int, int>> visited;
-        visited.insert({0, 0});
-        while(res.size() < k){
+        set<pair<int, int>> st;
+        st.insert({0, 0});
+        vector<vector<int>> res;
+        while(!pq.empty() && k--){
             auto it = pq.top();
             pq.pop();
 
+            int i = it.second.first, j = it.second.second;
+            res.push_back({nums1[i], nums2[j]});
             int sum = it.first;
-            int x = it.second.first, y = it.second.second;
-            res.push_back({nums1[x], nums2[y]});
-            if (y+1 < nums2.size() && !visited.count({x, y+1})) {
-                pq.push({nums1[x] + nums2[y+1], {x, y+1}});
-                visited.insert({x, y+1});
-            }
-            if (x+1 < nums1.size() && !visited.count({x+1, y})) {
-                pq.push({nums1[x+1] + nums2[y], {x+1, y}});
-                visited.insert({x+1, y});
-            }
+            if(j+1 < m && !st.count({i, j+1})){
+                pq.push({nums1[i]+nums2[j+1], {i, j+1}});
+                st.insert({i, j+1});
+            } 
+            if(i+1 < n && !st.count({i+1, j})){
+                pq.push({nums1[i+1]+nums2[j], {i+1, j}});
+                st.insert({i+1, j});
+            } 
         }
         return res;
     }

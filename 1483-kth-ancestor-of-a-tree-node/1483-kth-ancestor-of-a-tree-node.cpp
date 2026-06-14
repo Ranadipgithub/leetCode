ef3eb65a3a823cc2e23225ad257214a5596f3ec2
Rana Dutta <duttarana189@gmail.com>
2026-06-14 20:28:59 +0530

    Time: 53 ms (69.08%), Space: 122.6 MB (72.16%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -3,13 +3,13 @@ public:
     vector<vector<int>> up;
     int cols;
     TreeAncestor(int n, vector<int>& parent) {
-        cols = log2(n) + 1;
+        cols = log2(n)+1;
         up.assign(n, vector<int>(cols, -1));
-        for(int i = 0;i<n;i++){
-            up[i][0] = parent[i];
+        for(int node = 1;node<n;node++){
+            up[node][0] = parent[node];
         }
         for(int j = 1;j<cols;j++){
-            for(int node = 0;node < n;node++){
+            for(int node = 0;node<n;node++){
                 if(up[node][j-1] != -1)
                     up[node][j] = up[up[node][j-1]][j-1];
             }
@@ -18,10 +18,10 @@ public:
     
     int getKthAncestor(int node, int k) {
         for(int i = cols-1;i>=0;i--){
-            if(((1<<i)&k)){
+            if(((1<<i)&k)) {
+                if(up[node][i] == -1) return -1;
                 node = up[node][i];
-                if(node == -1) return -1;
-            } 
+            }
         }
         return node;
     }

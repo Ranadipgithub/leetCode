b122ef61f65fe555e655577299ccf6f2368cf336
Rana Dutta <duttarana189@gmail.com>
2026-06-18 10:35:49 +0530

    Time: 1656 ms (19.45%), Space: 553.8 MB (9.38%) - LeetHub

---
+++ 3844-longest-almost-palindromic-substring/3844-longest-almost-palindromic-substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(string &s, int i, int j){
+        if(i >= j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i][j] > 5) return INT_MAX;
+        int ans = INT_MAX;
+        if(s[i] == s[j]){
+            ans = min(ans, solve(s, i+1, j-1));
+        } else {
+            ans = min(ans, 1 + min(solve(s, i+1, j), solve(s, i, j-1)));
+        }
+        return dp[i][j] = ans;
+    }
+    int almostPalindromic(string s) {
+        int n = s.size();
+        dp.assign(n, vector<int>(n, -1));
+        solve(s, 0, n-1);
+        int ans = -1;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                int res = solve(s, i, j);
+                if(res == 1 || res == 0){
+                    ans = max(ans, j-i+1);
+                }
+            }
+        }
+        return ans;
+    }
+};

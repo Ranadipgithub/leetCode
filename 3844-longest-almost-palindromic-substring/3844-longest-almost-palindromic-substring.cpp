e177299d7bca91d109cded884718c047a088608e
Rana Dutta <duttarana189@gmail.com>
2026-06-18 11:15:09 +0530

    Time: 874 ms (45.83%), Space: 553.5 MB (13.2%) - LeetHub

--- 3844-longest-almost-palindromic-substring/3844-longest-almost-palindromic-substring.cpp
+++ 3844-longest-almost-palindromic-substring/3844-longest-almost-palindromic-substring.cpp
@@ -15,17 +15,29 @@ public:
     }
     int almostPalindromic(string s) {
         int n = s.size();
-        dp.assign(n, vector<int>(n, -1));
-        int ans = -1;
-        for(int i = 0;i<n;i++){
+        dp.assign(n, vector<int>(n, 0));
+        int ans = 1;
+        for(int i = n-1;i>=0;i--){
             for(int j = i;j<n;j++){
-                if(j-i+1 <= ans) continue;
-                int res = solve(s, i, j);
-                if(res == 1 || res == 0){
-                    ans = max(ans, j-i+1);
+                if(i == j) dp[i][j] = 0;
+                else if(i+1 == j) dp[i][j] = (s[i] == s[j])? 0: 1;
+                else {
+                    if(s[i] == s[j]) dp[i][j] = dp[i+1][j-1];
+                    else dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
                 }
+                if(dp[i][j] <= 1) ans = max(ans, j-i+1);
             }
         }
         return ans;
+        // for(int i = 0;i<n;i++){
+        //     for(int j = i;j<n;j++){
+        //         if(j-i+1 <= ans) continue;
+        //         int res = solve(s, i, j);
+        //         if(res == 1 || res == 0){
+        //             ans = max(ans, j-i+1);
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 };

7857c686b785318942e0ee67386c7424532cb01b
Rana Dutta <duttarana189@gmail.com>
2026-06-21 10:42:59 +0530

    Time: 52 ms (78.08%), Space: 106.7 MB (38.36%) - LeetHub

---
+++ 1840-maximum-building-height/1840-maximum-building-height.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxBuilding(int n, vector<vector<int>>& restrictions) {
+        restrictions.push_back({1, 0});
+        restrictions.push_back({n, n-1});
+        sort(begin(restrictions), end(restrictions));
+
+        int m = restrictions.size();
+
+        //Left To Right
+        for(int i = 1; i<m; i++) {
+            int diffDist = restrictions[i][0] - restrictions[i-1][0];
+            restrictions[i][1] = min(restrictions[i][1], restrictions[i-1][1] + diffDist);
+        }
+
+        //Right to Left
+        for(int i = m-2; i>=0; i--) {
+            int diffDist = restrictions[i+1][0] - restrictions[i][0];
+            restrictions[i][1] = min(restrictions[i][1], restrictions[i+1][1] + diffDist);
+        }
+
+        int result = 0;
+        for(int i = 1; i < m; i++) {
+            int leftPos = restrictions[i-1][0];
+            int leftHt  = restrictions[i-1][1];
+            int currPos = restrictions[i][0];
+            int currHt  = restrictions[i][1];
+            int d = currPos - leftPos;
+            int htDiff = abs(leftHt - currHt);
+
+            int peak = max(leftHt, currHt) + (d - htDiff)/2;
+            result = max(result, peak);
+        }
+        return result;
+    }
+};

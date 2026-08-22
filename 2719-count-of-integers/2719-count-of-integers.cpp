e294035accd08c2b6e602ce86189b66e3af9901a
Rana Dutta <duttarana189@gmail.com>
2026-08-22 23:06:03 +0530

    Time: 12 ms (85.14%), Space: 9.3 MB (45.9%) - LeetHub

--- 2719-count-of-integers/2719-count-of-integers.cpp
+++ 2719-count-of-integers/2719-count-of-integers.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    int dp[23][2][2][400];
-    const int mod = 1e9 + 7;
-    string subtractOne(string s) {
-        int i = s.size() - 1;
-        while(i >= 0 && s[i] == '0') {
-            s[i] = '9';
-            i--;
-        }
-        if(i >= 0) s[i]--;
-        return s;
-    }
-    int solve(string &s, int idx, int tight, int lz, int mini, int maxi, int curr){
+    int dp[23][2][200];
+    const int mod = 1e9+7;
+    int solve(string &s, int idx, int tight, int curr, int min_sum, int max_sum){
         if(idx == s.size()){
-            if(curr >= mini && curr <= maxi) return 1;
-            return 0;
+            return curr <= max_sum && curr >= min_sum;
         }
-        if(dp[idx][tight][lz][curr] != -1) return dp[idx][tight][lz][curr];
-        int lb = 0, ub = tight == 1? s[idx]-'0': 9;
+        if(dp[idx][tight][curr] != -1) return dp[idx][tight][curr];
+        int lb = 0;
+        int ub = tight == 1? s[idx]-'0': 9;
         int res = 0;
-        for(int dig = lb;dig<=ub;dig++){
-            if(lz && dig == 0){
-                res = (res + solve(s, idx+1, (tight && dig == ub), (lz && dig == 0), mini, maxi, curr))%mod;
-            } else {
-                res = (res + solve(s, idx+1, (tight && dig == ub), (lz && dig == 0), mini, maxi, curr+dig))%mod;
-            }
+        for(int dig = lb;dig <= ub;dig++){
+            res = (res + solve(s, idx+1, (tight && dig == s[idx]-'0'), curr+dig, min_sum, max_sum))%mod;
         }
-        return dp[idx][tight][lz][curr] = res%mod;
+        return dp[idx][tight][curr] = res%mod;
     }
     int count(string num1, string num2, int min_sum, int max_sum) {
         memset(dp, -1, sizeof(dp));
-        int ans1 = solve(num2, 0, 1, 1, min_sum, max_sum, 0);
-        num1 = subtractOne(num1);
+        int ans1 = solve(num1, 0, 1, 0, min_sum, max_sum);
         memset(dp, -1, sizeof(dp));
-        int ans2 = solve(num1, 0, 1, 1, min_sum, max_sum, 0);
-        return ((ans1 - ans2)%mod + mod)%mod;
+        int ans2 = solve(num2, 0, 1, 0, min_sum, max_sum);
+        int sum = 0;
+        for(char c: num1) sum += c - '0';
+        int add = sum >= min_sum && sum <= max_sum;
+        return (ans2 - ans1 + add + mod)%mod;
     }
 };

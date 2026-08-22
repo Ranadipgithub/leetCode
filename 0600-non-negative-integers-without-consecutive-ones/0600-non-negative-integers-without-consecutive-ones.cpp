2feb8bbed23fa65fdd39bba981d768cea93d9d4f
Rana Dutta <duttarana189@gmail.com>
2026-08-22 21:10:33 +0530

    Time: 0 ms (100%), Space: 8.2 MB (96.64%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int dp[32][2][2][3];
-    int solve(string &s, int idx, bool tight, bool lz, int prev){
+    int dp[32][2][3];
+    int solve(string &s, int idx, bool tight, int prev){
         if(idx == s.size()) return 1;
-        if(dp[idx][tight][lz][prev] != -1) return dp[idx][tight][lz][prev];
         int lb = 0;
-        int ub = tight == 1? s[idx]-'0' : 1;
+        int ub = (tight == 1)? s[idx]-'0': 1;
         int res = 0;
-        for(int dig = lb;dig<=ub;dig++){
-            if(prev == 1 && dig == 1) continue;
-            res += solve(s, idx+1, (dig == ub && tight), (lz && dig == 0), dig);
+        if(dp[idx][tight][prev] != -1) return dp[idx][tight][prev];
+        for(int dig = lb;dig <= ub; dig++){
+            if(dig == 1 && prev == 1) continue;
+            res += solve(s, idx+1, (tight && dig == ub), dig);
         }
-        return dp[idx][tight][lz][prev] = res;
+        return dp[idx][tight][prev] = res;
     }
     int findIntegers(int n) {
         string s = "";
@@ -19,8 +19,8 @@ public:
             s += n%2 + '0';
             n /= 2;
         }
-        memset(dp, -1, sizeof(dp));
         reverse(s.begin(), s.end());
-        return solve(s, 0, 1, 1, 2);
+        memset(dp, -1, sizeof(dp));
+        return solve(s, 0, 1, 2);
     }
 };

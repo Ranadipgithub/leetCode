6720d25bf207fced26925a047dd099f6593734f6
Rana Dutta <duttarana189@gmail.com>
2026-08-22 14:48:00 +0530

    Time: 4 ms (80.88%), Space: 8.1 MB (78.45%) - LeetHub

--- 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    // returns the no of strings without repeated digits
-    int dp[1024][11][2][2];
-    int solve(string &s, int mask, int idx, bool tight, bool lz){
-        // we are correctly able to form the string without repeating digits
+    int dp[10][2][2][1024];
+    int solve(string &s, int idx, bool tight, bool lz, int mask){
         if(idx == s.size()) return 1;
-        if(dp[mask][idx][tight][lz] != -1) return dp[mask][idx][tight][lz];
-        int lb = 0, ub = tight == 1? s[idx]-'0': 9;
+        int lb = 0;
+        int ub = (tight == 1) ? s[idx] - '0': 9;
         int res = 0;
-        for(int dig = lb;dig<=ub;dig++){
+        if(dp[idx][tight][lz][mask] != -1) return dp[idx][tight][lz][mask];
+        for(int dig = lb;dig <= ub;dig++){
             int temp = 1 << dig;
             if((temp & mask) != 0){
-                // the digit is repeated so we can't take this anymore
                 continue;
             } else if(dig == 0 && lz){
-                // for eg 040 it is unique, but if we mark the leading 0 in the mask
-                // this will mark the no as non unique, so if the digit is 0 and it is
-                // a leading 0 just move the mask forward without updating it
-                res += solve(s, mask, idx+1, (tight && dig == ub), (lz && dig == 0));
+                res += solve(s, idx+1, (tight & dig == ub), (lz && dig == 0), mask);
             } else {
-                res += solve(s, mask | (1<<dig), idx+1, (tight && dig == ub), (lz && dig == 0));
+                res += solve(s, idx+1, (tight & dig == ub), (lz && dig == 0), mask | (1 << dig));
             }
         }
-        return dp[mask][idx][tight][lz] = res;
+        return dp[idx][tight][lz][mask] = res;
     }
     int numDupDigitsAtMostN(int n) {
         string s = to_string(n);
-        memset(dp, -1, sizeof(dp));
         int mask = 0;
-        // total unique no from [1, n] = solve() - 1(removing the 0);
-        // no of digit with atleast one repeated digit = n - (solve()-1) = n+1-solve()
-        return n + 1 - solve(s, mask, 0, 1, 1);
+        memset(dp, -1, sizeof(dp));
+        return n+1 - solve(s, 0, 1, 1, mask);
     }
 };

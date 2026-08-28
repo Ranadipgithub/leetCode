a2235ace01227425c65b27d4f3a0b07b6647f9cf
Rana Dutta <duttarana189@gmail.com>
2026-08-28 20:35:31 +0530

    Time: 31 ms (52.63%), Space: 23.9 MB (21.59%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool solve(int i, int target, int val, int k, vector<int>&visited, vector<int>&nums){
-        if(k == 0) return true;
-        if(target == 0) return solve(0, val, val, k-1, visited, nums);
-        if(i >= nums.size()) return false;
-        bool take = false;
-        if(target >= nums[i] && !visited[i]){
-            visited[i] = true;
-            take = solve(i+1, target-nums[i], val, k, visited, nums);
-            if(take) return true;
-            visited[i] = false;
-        } 
-        bool skip = solve(i+1, target, val, k, visited, nums);
-        return take || skip;
+    vector<int> dp;
+    bool solve(int mask, int curr, int target, vector<int>&nums){
+        int n = nums.size();
+        if(mask == (1 << n) - 1) return true;
+        if(dp[mask] != -1) return dp[mask];
+        for(int i = 0;i<n;i++){
+            if((mask & (1 << i)) == 0){
+                if(curr + nums[i] <= target){
+                    int next_sum = (curr + nums[i]) % target;
+                    if(solve((mask | (1 << i)), next_sum, target, nums)) return  dp[mask] = true;
+                }
+            }
+        }
+        return dp[mask] = false;
     }
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum%k) return false;
-        int val = sum/k;
-        vector<int> visited(n, false);
-        sort(nums.rbegin(), nums.rend());
-        return solve(0, val, val, k, visited, nums);
+        if(sum % k) return false;
+        int target = sum / k;
+        dp.assign((1<<n), -1);
+        return solve(0, 0, target, nums);
     }
 };

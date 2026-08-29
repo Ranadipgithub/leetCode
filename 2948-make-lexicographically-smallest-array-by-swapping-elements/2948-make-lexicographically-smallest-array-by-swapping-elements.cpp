6e75548e82d72759cddae196cb6a6415c8304278
Rana Dutta <duttarana189@gmail.com>
2026-08-29 19:24:50 +0530

    Time: 103 ms (96.34%), Space: 134.3 MB (92.11%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        // 1 5 3 9 8
+        // 0 1 2 3 4
+        // (1, 0) (3, 2) (5, 1) (8, 4) (9, 3)
+        // 
         int n = nums.size();
-        vector<int> temp = nums;
-        sort(temp.begin(), temp.end());
-        unordered_map<int, list<int>> grpEl;
-        int grp = 0;
-        grpEl[grp].push_back(temp[0]);
-        unordered_map<int, int> grpNo;
-        grpNo[nums[0]] = grp;
-        for(int i = 1;i<n;i++){
-            if(abs(temp[i] - temp[i-1]) <= limit){
-                grpEl[grp].push_back(temp[i]);
-                grpNo[temp[i]] = grp;
-            } else {
-                grp++;
-                grpEl[grp].push_back(temp[i]);
-                grpNo[temp[i]] = grp;
-            }
-        }
-        vector<int> res(n);
+        vector<pair<int, int>> temp(n);
         for(int i = 0;i<n;i++){
-            int grp = grpNo[nums[i]];
-            res[i] = *(grpEl[grp].begin());
-            grpEl[grp].pop_front();
+            temp[i] = {nums[i], i};
+        }
+        sort(temp.begin(), temp.end());
+        int i = 0;
+        vector<int> res(n);
+        while(i < n){
+            int j = i + 1;
+            while(j < n && (temp[j].first -temp[j-1].first) <= limit) j++;
+            vector<int> indices;
+            indices.reserve(j-i);
+            for(int k = i;k<j;k++){
+                indices.push_back(temp[k].second);
+            }
+            sort(indices.begin(), indices.end());
+            for(int k = 0;k<indices.size();k++){
+                res[indices[k]] = temp[i+k].first;
+            }
+            i = j;
         }
         return res;
     }

01fc3281e776a0511dd7efebeabd0a38c79a29ee
Rana Dutta <duttarana189@gmail.com>
2026-09-15 11:31:02 +0530

    Time: 2464 ms (11.84%), Space: 377.9 MB (40.79%) - LeetHub

---
+++ 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    unordered_map<int, int> dp[10001][11];
+    int solve(int i, int j, int curr, vector<int>&nums, vector<int>&vals){
+        if(i == nums.size()){
+            if(j == vals.size()) return 0;
+            return 1e9;
+        }
+        if(j == vals.size()) return 1e9;
+        if(dp[i][j].count(curr)) return dp[i][j][curr];
+        int ans = solve(i+1, j, curr&nums[i], nums, vals);
+        if((curr & nums[i]) == vals[j]){
+            ans = min(ans, nums[i] + solve(i+1, j+1, (1<<17) -1, nums, vals));
+        }
+        return dp[i][j][curr] = ans;
+    }
+    int minimumValueSum(vector<int>& nums, vector<int>& andValues) {
+        int ans = solve(0, 0, (1 << 17) - 1, nums, andValues);
+        return ans >= 1e9? -1: ans;
+    }
+};

7837f27c037d658ef48aac1d578c25cb22ef0e63
Rana Dutta <duttarana189@gmail.com>
2026-09-15 11:58:24 +0530

    Time: 1227 ms (33.55%), Space: 311.1 MB (57.24%) - LeetHub

--- 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.cpp
+++ 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.cpp
@@ -1,7 +1,21 @@
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
 class Solution {
 public:
     // unordered_map<int, int> dp[10001][11];
-    unordered_map<long long, int> dp;
+    unordered_map<long long, int, custom_hash> dp;
     int solve(int i, int j, int curr, vector<int>&nums, vector<int>&vals){
         if(i == nums.size()){
             if(j == vals.size()) return 0;

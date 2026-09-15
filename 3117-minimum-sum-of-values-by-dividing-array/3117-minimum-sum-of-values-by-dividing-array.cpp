3864be8f495f61262cad82bf5ddc5ce3de881f73
Rana Dutta <duttarana189@gmail.com>
2026-09-15 11:41:28 +0530

    Time: 1082 ms (64.47%), Space: 311 MB (57.24%) - LeetHub

--- 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.cpp
+++ 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    unordered_map<int, int> dp[10001][11];
+    // unordered_map<int, int> dp[10001][11];
+    unordered_map<long long, int> dp;
     int solve(int i, int j, int curr, vector<int>&nums, vector<int>&vals){
         if(i == nums.size()){
             if(j == vals.size()) return 0;
             return 1e9;
         }
         if(j == vals.size()) return 1e9;
-        if(dp[i][j].count(curr)) return dp[i][j][curr];
-        int ans = solve(i+1, j, curr&nums[i], nums, vals);
+        long long key = (long long) curr << 18 | j << 14 | i;
+        if(dp.count(key)) return dp[key];
+        // if(dp[i][j].count(curr)) return dp[i][j][curr];
         if((curr & nums[i]) < vals[j]) return 1e9;
+        int ans = solve(i+1, j, curr&nums[i], nums, vals);
         if((curr & nums[i]) == vals[j]){
             ans = min(ans, nums[i] + solve(i+1, j+1, (1<<17) -1, nums, vals));
         }
-        return dp[i][j][curr] = ans;
+        return dp[key] = ans;
     }
     int minimumValueSum(vector<int>& nums, vector<int>& andValues) {
         int ans = solve(0, 0, (1 << 17) - 1, nums, andValues);
